b093d6edb6d82fdedb7243f4f6c660fbadf05ae1
sonuabrahamk <sonu.abrahamk@gmail.com>
2017-03-01 09:57:44 +0530

    mba page

---
+++ mba/php/newsAndUpdates.php
@@ -0,0 +1,19 @@
+<div class="innerDiv">
+    <h2>OUR PROMISE</h2>
+    <p>100 % results &amp; placements with inspiration, innovation motivation.</p>
+</div>
+
+<div class="innerDiv">
+    <h2>OUR BIG HAIRY AUDACIOUS GOAL</h2>
+    <p>To educate and inspire 10 million people and become a 3 billion dollar organization by 2025.</p>
+</div>
+
+<div class="innerDiv" style="border: none;">
+    <h2>OUR CORE PURPOSE</h2>
+    <p>To provide value based services to the people for nurturing their skills, knowledge and capabilities to face dynamically change the world with innovation, inspiration &amp; motivation.</p>
+</div>
+
+
+
+
+
