765eb0e8779ef3a63883bde70aace698bff78345
sonuabrahamk <sonu.abrahamk@gmail.com>
2017-03-30 10:47:17 +0530

    mba page

--- mba/index.php
+++ mba/index.php
@@ -21,17 +21,22 @@
 		<link rel="stylesheet" href="../css/navTabs.css">
 		<link rel="stylesheet" href="../css/animate.css">
 		<link rel="stylesheet" href="../css/fakeLoader.css">
+
+
 		<!--        <link rel="stylesheet" href="css/res-navtabs.css">-->
 		<!--custom css-->
 		<link href="../css/custom.css" rel="stylesheet">
 		<link href="../css/innerpage.css" rel="stylesheet">
 		<link href="css/mba.css" rel="stylesheet">
+        
 		<!-- javascript -->
 		<script src="../js/jquery-1.11.0.min.js"></script>
 		<script src="../bootstrap/js/bootstrap.min.js"></script> 
 		<script src="../js/respond.js"></script>
 		<script src="../js/custom.js"></script>
-		<script src="../js/fakeLoader.js"></script>
+        <script src="../js/fakeLoader.js"></script>
+        <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.js"></script>
 
 	</head>
 	<body>
@@ -91,6 +96,24 @@
                                 </div>
                                 <div class="clearfill"></div>
                             </div>
+                            
+                            
+                            <div class="row" >
+                                <div class="col-lg-9">
+                                    <div class="content-spl myWelcome wow fadeIn" style="margin-bottom:1px;">
+                                        <?php include ('php/welcome.php'); ?>
+                                    </div>
+                                    <div class="content-spl myNav wow fadeIn">
+                                        <?php include ('php/accordion-pill-tabs.php'); ?>
+                                        <div class="clearfill"></div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3">
+                                    <div class="content-spl wow fadeIn">
+                                        <?php include ('php/testimonial.php'); ?>
+                                    </div>
+                                </div>
+                            </div>
 
 						</div>
 
@@ -105,7 +128,6 @@
 				</div> <!--/st-content-inner--> 
 			</div><!-- this is the wrapper for the content--> 
 		</div> <!--st-pusher--> 
-		</div> <!--st-container--> 
 
 	<script src="../js/cbpFWTabs.js"></script>
 	<script>
@@ -137,5 +159,16 @@
 		}
 	</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 
+        
+    <script>
+        
+        $(document).ready(function(){
+            
+            
+            
+        });
+        
+    </script>
+        
 	</body>   
 </html>
