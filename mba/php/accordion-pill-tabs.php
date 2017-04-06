5496c5f15c424fa72289ed17157f38311e994549
Sonu Abraham K <sonu.abrahamk@gmail.com>
2017-04-06 08:54:51 +0530

    Mba Change

--- mba/php/accordion-pill-tabs.php
+++ mba/php/accordion-pill-tabs.php
@@ -1,163 +1,175 @@
 <style>
-	.FavStyles a:hover,.FavStyles a:active{
-		color: black;
-		text-decoration: none;
-	}
 	.tab-link-exam:active{
 		color:#1c6fb5;
 		text-decoration: none;
 	}
-
+    
+    .tab-pane h4{
+        color: #34495e;
+        font-size: 1.2em;
+        margin-top: 5px;
+        margin-bottom: 5px;
+    }
+    
+    .tab-pane hr{
+        margin: 5px 0;
+    }
+    
+    .FavStyles a:hover{
+        text-decoration: none;
+        color: #34495e;
+    }
+    
 </style>
-<ul class="nav nav-pills nav-stacked col-lg-3 col-md-3 col-sm-12 col-xs-12">
+<ul class="nav nav-pills nav-stacked col-lg-4 col-md-4 col-sm-12 col-xs-12">
 	<li class="active">
-		<a href="#link-1" data-toggle="tab" aria-expanded="true">My Favourites</a>
+        <a href="#link-1" data-toggle="tab" aria-expanded="true"><i class="fa fa-hand-o-right"></i>&nbsp; My Favourites</a>
 	</li>
 	<li class="">
-		<a href="#link-2" data-toggle="tab" aria-expanded="false">IQAC</a>
+        <a href="#link-2" data-toggle="tab" aria-expanded="false"><i class="fa fa-hand-o-right"></i>&nbsp; IQAC</a>
 	</li>
 	<li class="">
-		<a href="#link-3" data-toggle="tab" aria-expanded="false">Jayantian Special</a>
+        <a href="#link-3" data-toggle="tab" aria-expanded="false"><i class="fa fa-hand-o-right"></i>&nbsp; Jayantian Special</a>
 	</li>
 	<li class="">
-		<a href="https:\\www.google.com" class="tab-link-exam" aria-expanded="false">Examination Fee</a>
+        <a href="https:\\www.google.com" class="tab-link-exam" aria-expanded="false"><i class="fa fa-hand-o-right"></i>&nbsp; Examination Fee</a>
 	</li>
 	<li class="">
-		<a href="#link-5" data-toggle="tab" aria-expanded="false">NACC Peer Team Visit</a>
+        <a href="#link-5" data-toggle="tab" aria-expanded="false"><i class="fa fa-hand-o-right"></i>&nbsp; NACC Peer Team Visit</a>
 	</li>
 </ul>
-<div id="myTabContent" class="tab-content col-lg-9 col-md-9 col-sm-12 col-xs-12">
+<div id="myTabContent" class="tab-content col-lg-8 col-md-8 col-sm-12 col-xs-12">
 
 	<div class="tab-pane fade active in" id="link-1">
-        <h4 style="margin: 2px; font-size: 24px;">My Favourites</h4><hr style="margin: 10px 0;margin-bottom: 30px;" />
+        <h4>My Favourites</h4><hr />
 		<div class="col-lg-4 col-xs-12" style="color:#1c6fb5">
-			<h4 class="FavStyles">
-				<a href="#">
+			<h5 class="FavStyles">
+                <a href="#">
 					<i class="fa fa-calendar"></i>&nbsp;&nbsp;Academic Calendar
 				</a>
-			</h4>
+			</h5>
 		</div>
 		<div class="col-lg-4 col-xs-12" style="color:#1c6fb5">
-			<h4 class="FavStyles">
-				<a href="#">
+			<h5 class="FavStyles">
+                <a href="#">
 					<i class="fa fa-picture-o"></i>&nbsp;&nbsp;Gallery
 				</a>
-			</h4>
+			</h5>
 		</div>
 		<div class="col-lg-4 col-xs-12" style="color:#1c6fb5">
-			<h4 class="FavStyles">
-				<a href="#">
+			<h5 class="FavStyles">
+                <a href="#">
 					<i class="fa fa-file-text"></i>&nbsp;&nbsp;Notice
 				</a>
-			</h4>
+			</h5>
 		</div>
 		<div class="col-lg-4 col-xs-12" style="color:#1c6fb5">
-			<h4 class="FavStyles">
-				<a href="#">
+			<h5 class="FavStyles">
+                <a href="#">
 					<i class="fa fa-flag"></i>&nbsp;&nbsp;Events
 				</a>
-			</h4>
+			</h5>
 		</div>
 
 		<div class="col-lg-4 col-xs-12" style="color:#1c6fb5">
-			<h4 class="FavStyles">
-				<a href="#">
+            <h5 class="FavStyles">
+                <a href="#">
 					<i class="fa fa-lightbulb-o"></i>&nbsp;&nbsp;C M S
 				</a>
-			</h4>
+			</h5>
 		</div>
 		<div class="col-lg-4 col-xs-12" style="color:#1c6fb5">
-			<h4 class="FavStyles">
+            <h5 class="FavStyles">
 				<a href="#">
 					<i class="fa fa-envelope"></i>&nbsp;&nbsp;Mail
 				</a>
-			</h4>
+			</h5>
 		</div>
 		<div class="col-lg-4 col-xs-12" style="color:#1c6fb5">
-			<h4 class="FavStyles">
+            <h5 class="FavStyles">
 				<a href="#">
 					<i class="fa fa-phone"></i>&nbsp;&nbsp;Support
 				</a>
-			</h4>
+			</h5>
 		</div>
 
 		<div class="col-lg-4 col-xs-12" style="color:#1c6fb5">
-			<h4 class="FavStyles">
+			<h5 class="FavStyles">
 				<a href="#">
 					<i class="fa fa-globe"></i>&nbsp;&nbsp;Campus Connect
 				</a>
-			</h4>
+			</h5>
 		</div>
 	</div>
 
 	<div class="tab-pane fade" id="link-2">
-        <h4 style="margin: 2px; font-size: 24px;">IQAC</h4><hr style="margin: 10px 0;margin-bottom: 30px;" />
+        <h4>IQAC</h4><hr />
 		<div class="col-lg-4 col-xs-12" style="color:#1c6fb5">
-			<h4 class="FavStyles">
+			<h5 class="FavStyles">
 				<a href="#">
 					<i class="fa fa-hand-o-right"></i>&nbsp;&nbsp;Functions
 				</a>
-			</h4>
+			</h5>
 		</div>
 		<div class="col-lg-4 col-xs-12" style="color:#1c6fb5">
-			<h4 class="FavStyles">
+			<h5 class="FavStyles">
 				<a href="#">
 					<i class="fa fa-hand-o-right"></i>&nbsp;&nbsp;Composition
 				</a>
-			</h4>
+			</h5>
 		</div>
 		<div class="col-lg-4 col-xs-12" style="color:#1c6fb5">
-			<h4 class="FavStyles">
+			<h5 class="FavStyles">
 				<a href="#">
 					<i class="fa fa-hand-o-right"></i>&nbsp;&nbsp;Activites
 				</a>
-			</h4>
+			</h5>
 		</div>
 		<div class="col-lg-4 col-xs-12" style="color:#1c6fb5">
-			<h4 class="FavStyles">
+			<h5 class="FavStyles">
 				<a href="#">
 					<i class="fa fa-hand-o-right"></i>&nbsp;&nbsp;AQAR
 				</a>
-			</h4>
+			</h5>
 		</div>
 	</div>
 
 	<div class="tab-pane fade" id="link-3">
-        <h4 style="margin: 2px; font-size: 24px;">Jayantian Special</h4><hr style="margin: 10px 0;margin-bottom: 30px;" />
+        <h4>Jayantian Special</h4><hr />
 		<div class="col-lg-4 col-xs-12 tab-pane-inner" style="color:#1c6fb5;">
-			<h4 class="FavStyles">
+			<h5 class="FavStyles">
 				<a href="#">
 					<i class="fa fa-hand-o-right"></i>&nbsp;&nbsp;Vichaarmanthan
 				</a>
-			</h4>
+			</h5>
 		</div>
 		<div class="col-lg-4 col-xs-12 tab-pane-inner" style="color:#1c6fb5">
-			<h4 class="FavStyles">
+			<h5 class="FavStyles">
 				<a href="#">
 					<i class="fa fa-hand-o-right"></i>&nbsp;&nbsp;Glimpses
 				</a>
-			</h4>
+			</h5>
 		</div>
 		<div class="col-lg-4 col-xs-12 tab-pane-inner" style="color:#1c6fb5">
-			<h4 class="FavStyles">
+			<h5 class="FavStyles">
 				<a href="#">
 					<i class="fa fa-hand-o-right"></i>&nbsp;&nbsp;Our Culture
 				</a>
-			</h4>
+			</h5>
 		</div>
 		<div class="col-lg-4 col-xs-12 tab-pane-inner" style="color:#1c6fb5">
-			<h4 class="FavStyles">
+			<h5 class="FavStyles">
 				<a href="#">
 					<i class="fa fa-hand-o-right"></i>&nbsp;&nbsp;Faculty Development
 				</a>
-			</h4>
+			</h5>
 		</div>
 		<div class="col-lg-4 col-xs-12 tab-pane-inner" style="color:#1c6fb5">
-			<h4 class="FavStyles">
+			<h5 class="FavStyles">
 				<a href="#">
 					<i class="fa fa-hand-o-right"></i>&nbsp;&nbsp;Value Instruction classes
 				</a>
-			</h4>
+			</h5>
 		</div>
 
 	</div>
@@ -167,13 +179,13 @@
 	</div>
 
 	<div class="tab-pane fade" id="link-5">
-        <h4 style="margin: 2px; font-size: 24px;">NAAC Peer Team Visit</h4><hr style="margin: 10px 0;margin-bottom: 30px;" />
+        <h4>NAAC Peer Team Visit</h4><hr />
 		<div class="col-lg-12 col-xs-12 tab-pane-inner" style="color:#1c6fb5;">
-			<h4 class="FavStyles">
+			<h5 class="FavStyles">
 				<a href="#">
 					<i class="fa fa-hand-o-right"></i>&nbsp;&nbsp;NACC Peer Team Vist October 2015 Photos
 				</a>
-			</h4>
+			</h5>
 		</div>
 
 	</div>
