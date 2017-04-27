77b74fc8405386b298c3dc13e84b38d59bce32dd
Sonu Abraham K <sonu.abrahamk@gmail.com>
2017-04-27 11:39:22 +0530

    Temp

---
+++ academics/College-Arts-Science-Commerce/php/deanary.php
@@ -0,0 +1,39 @@
+
+<h4 style="background-image: url(../../images/headline_bg.jpg);
+    background-repeat: no-repeat;
+    background-position: 0px bottom;
+           padding-bottom: 7px;padding-top:15px;"><i class="fa fa-bookmark" style="color:#2980b9;"></i>
+    <span style="font-size: 20px;">&nbsp;The <strong style="color:#2980b9;font-size: 24px;">Deanaries</strong></span>
+</h4>
+<!--<hr />-->
+<div class="row">
+    
+    <div class="col-lg-4 deanary">
+        <div class="iconDiv">
+            <i class="fa fa-folder-open-o" aria-hidden="true"></i>
+        </div>
+        <h3>Faculty of Commerce</h3>
+        <p>Sample Text to be displayed for deanery.Sample Text to be displayed for deanery.</p>
+        <a href="#">Read More</a>
+    </div>
+    
+    <div class="col-lg-4 deanary">
+        <div class="iconDiv">
+            <i class="fa fa-folder-open-o" aria-hidden="true"></i>
+        </div>
+        <h3>Faculty of Sciences</h3>
+        <p>Sample Text to be displayed for deanery.Sample Text to be displayed for deanery.</p>
+        <a href="#">Read More</a>
+    </div>
+    
+    <div class="col-lg-4 deanary">
+        <div class="iconDiv">
+            <i class="fa fa-folder-open-o" aria-hidden="true"></i>
+        </div>
+        <h3>Faculty of Humanities</h3>
+        <p>Sample Text to be displayed for deanery.Sample Text to be displayed for deanery.</p>
+        <a href="#">Read More</a>
+    </div>
+
+</div>
+
