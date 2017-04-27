89e32ed7bd8647723582d5a97481f78c6515b6cc
JIJO JOSEPH <jijoputhiyath@gmail.com>
2017-04-27 15:23:43 +0530

    new change layout

--- events/loadEvent.php
+++ events/loadEvent.php
@@ -2,5 +2,5 @@
 	$year = $_REQUEST['year'];
 	$month = $_REQUEST['month'];
 //	include $year."/".$month."/index.php";
-	if(!(@include $year."/".$month."/index.php")) echo "<div class='col-lg-12 col-md-12 col-sm-12 col-xs-12 event-tab'><div><b>No News To be  Displayed </b><br/></div></div>";
+	if(!(@include $year."/".$month."/index.php")) echo "<div class='col-lg-12 col-md-12 col-sm-12 col-xs-12 event-tab'><div><b>No News To be  Displayed for month ".$month." </b><br/></div></div>";
 ?>
