33324445ab8ff2cbaf26d911843feea68c995ef1
JIJO JOSEPH <jijoputhiyath@gmail.com>
2017-05-05 11:15:09 +0530

    notice page

--- notice/2017/January/index.php
+++ notice/2017/January/index.php
@@ -1,98 +1,173 @@
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 event-tab">
+    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+        <div class="panel panel-default">
+            <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                <div class="panel-heading" role="tab" id="headingOne">
+                    <h4 class="panel-title">
 
-<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 event-tab">
-	
-	<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-  <div class="panel panel-default">
-    <div class="panel-heading" role="tab" id="headingOne">
-      <h4 class="panel-title">
-        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          Collapsible Group Item #1
-        </a>
-      </h4>
+                        April 21<sup>st</sup>
+                        <i class="fa fa-caret-right pull-right "></i>
+                    </h4>
+                </div>
+            </a>
+            <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                <div class="panel-body">
+                    
+        
+		  <p><b>For II Semester PG classes (Except MCA &amp; MBA)</b><br>
+			  Hall Ticket of the End Semester Theory Examination shall be issued by the Class Animators on 27th April, 2017 at 10:00 a.m.</p>
+
+		  <p><b>The documents required to collect the Hall Ticket are as follows:</b><br>
+
+1.Clear all Dues Fine/ Tuition Fee/ Lab  &amp; Library dues<br>
+			  2.ID Card.</p>
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseOne">
+                <div class="panel-heading" role="tab" id="headingOne">
+                    <h4 class="panel-title">
+
+                        April 03<sup>rd</sup>
+                        <i class="fa fa-caret-right pull-right "></i>
+                    </h4>
+
+                </div>
+            </a>
+            <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+                <div class="panel-body">
+                   
+       
+        
+<p><b>UG Classes and PG IV Semester classes only</b><br>
+List of attendance shortage students who have to attend the remedial classes and the venue for the remedial classes will be announced in the college notice board by 3:00 p.m. today (3/4/2017).</p>
+<p>Hall Ticket of the End Semester Theory Examination for students who are not having attendance shortage (85 Percentage or more) as on 3rd April, 2017 shall be issued by the Class Animators today, 3rd April, 2017  between 2:00 p.m. and 4:00 p.m.</p>
+<p>Hall Tickets of attendance shortage students shall be issued on 17thApril, 2017 between 2:00 p.m. and 4:00 p.m. provided these students have attended the remedial classes without missing single session on the specified dates.</p>
+<p><b>For VI Semester UG classes</b><br>
+<b>CECR: Issue of No Objection Certificate</b><br>
+All the UG students who have been placed during this academic year (Gate 2016-17) are required to be present in Mini Auditorium I, 5th Floor, PG Block, today (03/04/2017) at 3.00 p.m. The agenda of the meeting is the issue of No Objection Certificate which they need to carry to the company at the time of joining. Kindly note that students who are absent will not be entertained later.</p>
+<p><b>For all UG classes</b><br>
+<b>Certificate Course on Electronic Circuit Designing</b><br>
+The Department of Computer Science (UG) is organizing a Certificate Course on Electronic Circuit Designing. The certificate course is open to students of Kristu Jayanti College and the members of the neighborhood. For non-jayantian applicants, the minimum qualification will be a pass in 8th standard. The total duration of the course will be 20 hours and is scheduled in May 2017.  The number of seats are limited. Interested students may contact Mr. Nagendra, Faculty Member at the Electronics Lab, I Floor, Main Block and register on or before 20th April, 2017.</p>
+
+<p>Students who are having Attendance Percentage greater than or equal to 85% as on  April 04, 2017, 10.00 am should report to the Controller of Examination and collect Hall ticket from 11.00 am  onwards.</p>
+
+<p>Students having attendance percentage between 75 and 80 percentage have to be present for the remedial classes from tomorrow(4-Apr-17) onwards till 12th of April 2017 without missing any single session.</p>
+
+<p>Students having attendance percentage between 80 and 85 percentage have to be present for the remedial classes from tomorrow(4-Apr-17) onwards till 8th of April 2017 without missing any single session.</p>
+
+
+<p>Students having attendance percentage lesser than 75 are not eligible to appear for the End Semester Examinations.</p>
+<p><b>Venue for Remedial Classes</b></p>
+<table border="1" cellspacing="2" cellpadding="0" align="center">
+  <tbody><tr>
+    <td width="328" valign="top"><p><strong>Class</strong></p></td>
+    <td width="279" valign="top"><p><strong>Room No.</strong></p></td>
+  </tr>
+  <tr>
+    <td width="328" valign="top"><p>B Com II Semester</p></td>
+    <td width="279" valign="top"><p>201</p></td>
+  </tr>
+  <tr>
+    <td width="328" valign="top"><p>B Com IV Semester</p></td>
+    <td width="279" valign="top"><p>202</p></td>
+  </tr>
+  <tr>
+    <td width="328" valign="top"><p>B Com VI Semester</p></td>
+    <td width="279" valign="top"><p>203</p></td>
+  </tr>
+  <tr>
+    <td width="328" valign="top"><p>BBA II Semester</p></td>
+    <td width="279" valign="top"><p>301</p></td>
+  </tr>
+  <tr>
+    <td width="328" valign="top"><p>BBA    IV &amp; VI Semesters</p></td>
+    <td width="279" valign="top"><p>302</p></td>
+  </tr>
+  <tr>
+    <td width="328" valign="top"><p>BCA II Semester</p></td>
+    <td width="279" valign="top"><p>403</p></td>
+  </tr>
+  <tr>
+    <td width="328" valign="top"><p>BCA    IV &amp; VI Semesters</p></td>
+    <td width="279" valign="top"><p>402</p></td>
+  </tr>
+  <tr>
+    <td width="328" valign="top"><p>BA II Semester</p></td>
+    <td width="279" valign="top"><p>409</p></td>
+  </tr>
+  <tr>
+    <td width="328" valign="top"><p>BA IV Semester</p></td>
+    <td width="279" valign="top"><p>509</p></td>
+  </tr>
+  <tr>
+    <td width="328" valign="top"><p>BA VI Semester</p></td>
+    <td width="279" valign="top"><p>520</p></td>
+  </tr>
+  <tr>
+    <td width="328" valign="top"><p>BCA II Semester</p></td>
+    <td width="279" valign="top"><p>403</p></td>
+  </tr>
+  <tr>
+    <td width="328" valign="top"><p>BCA    IV &amp; VI Semesters</p></td>
+    <td width="279" valign="top"><p>402</p></td>
+  </tr>
+  <tr>
+    <td width="328" valign="top"><p>B.Sc.    (CSMS &amp; CSME) II / IV &amp; VI Semester</p></td>
+    <td width="279" valign="top"><p>404</p></td>
+  </tr>
+  <tr>
+    <td width="328" valign="top"><p>B.Sc.    (BT &amp; MB) II / IV &amp; VI Semester</p></td>
+    <td width="279" valign="top"><p>308</p></td>
+  </tr>
+</tbody></table>
+
+
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseOne">
+                <div class="panel-heading" role="tab" id="headingOne">
+                    <h4 class="panel-title">
+
+                        April 1<sup>st</sup>
+                        <i class="fa fa-caret-right pull-right "></i>
+                    </h4>
+                </div>
+            </a>
+            <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+                <div class="panel-body">
+                    
+        
+        
+<p><b>For all UG &amp; IV Sem PG classes</b><br>
+Hall Ticket of the End Semester Theory Examination for students who are not having attendance shortage (85 Percentage or more) as on 3rd April, 2017 shall be issued by the Class Animators on 3rd April, 2017  between 2:00 p.m. and 4:00 p.m.</p>
+<p>Hall Tickets of attendance shortage students shall be issued on 17th April, 2017 between 2:00 p.m. and 4:00 p.m. provided these students have attended the remedial classes without missing single session on the specified dates.</p>
+<p><b>For all UG &amp; PG classes</b><br>
+<b>Scholarship</b><br>
+<b>E Pass &amp; SC/ST Scholarship</b><br>
+Students who have applied for E Pass &amp; SC/ST scholarship can collect their scholarship from Counter No. 1 (UG Office) as per the schedule displayed on the notice board.</p>
+<p><b>Minority Scholarship</b><br>
+Students who have applied for Post Matric and Merit cum Means scholarship are hereby informed to keep their Bank account active as the transferring of scholarship amount has been started.</p>
+<p><b>For all UG classes</b><br>
+<b>Certificate Course on Electronic Circuit Designing</b><br>
+The Department of Computer Science (UG) is organizing a Certificate Course on Electronic Circuit Designing. The certificate course is open to students of Kristu Jayanti College and the members of the neighborhood. For non-jayantian applicants, the minimum qualification will a pass in 8th standard. The total duration of the course will be 20 hours and is scheduled in May 2017.  The number of seats are limited. Interested students may contact Mr. Nagendra, Faculty Member at the Electronics Lab, I Floor, Main Block and register on or before 20th April, 2017.</p>
+<p><b>For VI Semester UG classes</b><br>
+<b>CECR: Issue of No Objection Certificate</b><br>
+All the UG students who have been placed during this academic year (Gate 2016-17) are required to be present in Mini Auditorium I, 5th Floor, PG Block on 03/04/2017 (Monday) at 3.00 p.m. The agenda of the meeting is the issue of No Objection Certificate which they need to carry to the company at the time of joining. Kindly note that students who are absent on 03/04/2017 will not be entertained later.</p>
+<p>Guidelines regarding application for Special Supplementary Examinations for the students who are having supplementary for courses of fifth semester (only) is published in the Examination Office notice board.  Last date for application for the same is 8th April, 2017.</p>
+<p><b>All PG IV Semester Classes (except MCA IV Semester)</b><br>
+Guidelines regarding application for Special Supplementary Examinations for the students who are having supplementary for courses of third semester (only) is published in the Examination Office notice board.  Last date for application for the same is 8th April, 2017.</p>
+<p><b>MBA/PGDM VI Trimester Classes</b><br>
+Guidelines regarding application for Special Supplementary Examinations for the students who are having supplementary for courses of fourth or fifth trimesters (only) is published in the Examination Office notice board.  Last date for application for the same is 8th April, 2017.</p>
+<p><b>M Sc. (Bio/Micro/Psych) II Semester Classes</b><br>
+Term Practical Examination Time Table is published in the college notice boards and college website.</p>
+
+ 
+                </div>
+            </div>
+        </div>
     </div>
-    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-      <div class="panel-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-	
-	
-	
-	
-	
-	<div class="panel panel-primary">
-		<div class="panel-heading">MDP - (PGDM &amp; MBA)
-            <b class="panel-title pull-right">07<sup>th</sup> </b>
-            <div style="clear:both"></div>
-        </div>
-		<div class="panel-body">
-			The School of Management (PGDM &amp; MBA)Has organized a MDP on Digital Marketing Strategies <a class="event-links" data-year="2017" data-month="January" href="mdp_digital_marketing_strategies.php">Read More</a> 
-		</div>
-	</div>
-	<div class="panel panel-primary">
-		<div class="panel-heading">Social Responsibility Week
-            <b class="panel-title pull-right">9<sup>th</sup> - 14<sup>th</sup> </b>
-            <div style="clear:both"></div>
-        </div>
-        <div class="panel-body">
-			Department of Management Has conducted Social Responsibility Week on Dates between January 9th - 14th <a class="event-links" data-year="2017" data-month="January" href="social_responsibility_week.php">Read More</a>
-		</div>
-	</div>
-    <div class="panel panel-primary">
-		<div class="panel-heading">
-            32<sup>nd</sup>Edition Vichaarmanthan
-            <b class="panel-title pull-right">21<sup>st</sup> </b>
-            <div style="clear:both"></div>
-        </div>
-		<div class="panel-body">
-			32nd Edition Vichaarmanthan An Interaction with Global and Indian Visionaries An Interactive Session with Honourable Justice Kurian Joseph Judge of Supreme Court of India 10 AM at SKE Auditorium.
-		</div>
-	</div>
-	
-</div>
-<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 event-tab">
-	<div class="panel panel-primary">
-		<div class="panel-heading">
-            30<sup>th</sup> Edition - Vichaarmanthan
-            <b class="panel-title pull-right">10<sup>th</sup> </b>
-            <div style="clear:both"></div>
-        </div>
-		<div class="panel-body">
-			30th Edition Vichaarmanthan An Interaction with Global and Indian Visionaries An Interactive Session with Shri T.P. Sreenivasan, (Indian Foreign Service, 1967) Former Permanent Representative of India to the United Nations <a class="event-links" data-year="2017" data-month="January" href="vichaarmanthan_sreenivasan.php">Read More</a>
-		</div>
-	</div>
-    <div class="panel panel-primary">
-		<div class="panel-heading">
-            8<sup>th</sup> IQAC Conference
-            <b class="panel-title pull-right">19<sup>th</sup> &amp; 20<sup>th</sup> </b>
-            <div style="clear:both"></div>
-        </div>
-		<div class="panel-body">
-			I-Con 8th IQAC Conference on 'Engendering Technological Excellence in Higher Education' <a class="event-links" data-year="2017" data-month="January" href="8th_iqac_conference.php">Read More</a>
-		</div>
-	</div>
-    
-</div>
-<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 event-tab">
-	<div class="panel panel-primary">
-		<div class="panel-heading">
-            30<sup>th</sup> Edition - Vichaarmanthan
-            <b class="panel-title pull-right">16<sup>th</sup> </b>
-            <div style="clear:both"></div>
-        </div>
-		<div class="panel-body">
-			31st Edition Vichaarmanthan An Interaction with Global and Indian Visionaries An Interactive Session with Shri. Suresh Prabhu Honourable Union Minister for Railways Govt. of India 11.30 AM at SKE Auditorium. <a href="http://events.kristujayanti.edu.in/login.php" target="_blank">[Enroll Now]</a>
-		</div>
-	</div>
-    <div class="panel panel-primary">
-		<div class="panel-heading">
-            Vanijya Darpan 2017
-            <b class="panel-title pull-right">20<sup>th</sup> </b>
-            <div style="clear:both"></div>
-        </div>
-		<div class="panel-body">
-			Department of Commerce "Vanijya Darpan 2017 - A Business Skills Exhibition"  <a class="event-links" data-year="2017" data-month="January" href="vanijya_darpan_2017.php">Read More</a>
-		</div>
-	</div>
 </div>
