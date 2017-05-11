f05cefa8e7454292fd14400ae998867c19399fa4
PraveenKumarkjc <pravisweets@gmail.com>
2017-05-11 14:22:33 +0530

    highlights section
    highlights recent updates.

--- home/php/highLights.php
+++ home/php/highLights.php
@@ -8,47 +8,210 @@
 <div class="frame-testimonials" style="margin-top: 10px;">
     <ul id="frameHighLights" class="owl-carousel">
         <li>
-            <img src="../images/socialwork_camp_2017.jpg" class="img-responsive"/>
+            <img src="../highlights/2017/socialwork_camp_2017.jpg" class="img-responsive"/>
             <span class="date">30<sup>th</sup> March 2017</span>
             <div class="quote">
-                <a href="#"><h4>Social Work Camp 2017<br/><small>The Social Work Camp organized by the Department of Social Work, Kristu Jayanti College, Bangalore..</small></h4></a>
+                <a href="../highlights/social_work_camp_2017.php"><h4>Social Work Camp 2017<br/><small>
+				The Social Work Camp organized by the Department of Social Work, Kristu Jayanti College, Bangalore..</small></h4></a>
             </div>
         </li>
         <li>
-            <img src="../images/data_analysis.jpg" class="img-responsive"/>
+            <img src="../highlights/2017/socialwork_camp_2017.jpg" class="img-responsive"/>
             <span class="date">20<sup>th</sup> March 2017</span>
             <div class="quote">
-              <a href="#"><h4>FDP on Data Analysis In Natural Sciences<br/><small>The Department of Life Sciences conducted Faculty Development Programme on Data Analysis..</small></h4></a>
+              <a href="#"><h4>FDP on Data Analysis In<br> Natural Sciences<br/><small>The Department of Life Sciences conducted Faculty Development Programme on Data Analysis..</small></h4></a>
             </div>
         </li>
         <li>
-            <img src="../images/sensitivity.jpg" class="img-responsive"/>
+            <img src="../highlights/2017/sensitivity.jpg" class="img-responsive"/>
             <span class="date">24 - 25 March 2017</span>
             <div class="quote">
-                <a href="#"><h4>Workshop on Gender Sensitivity<br/><small>The Department of Social Work organized a two day workshop on Gender Sensitivity"..</small></h4></a>
+                <a href="#"><h4>Workshop on Gender<br>  Sensitivity<br/><small>The Department of Social Work organized a two day workshop on Gender Sensitivity"..</small></h4></a>
             </div>
         </li>
         <li>
-            <img src="../images/socail_work_day.jpg" class="img-responsive"/>
+            <img src="../highlights/2017/socail_work_day.jpg" class="img-responsive"/>
             <span class="date">24<sup>th</sup> March 2017</span>
             <div class="quote">
-                <a href="#"><h4>World Social Worker's Day Celebration<br/><small>The Department of Social Work organized a Program followed by an exhibition on account of World Social Workers Day on the  in the college premises.</small></h4></a>
+                <a href="#"><h4>World Social Worker's Day<br> Celebration<br/><small>The Department of Social Work organized a Program followed by an exhibition on account of World Social Workers Day on the  in the college premises.</small></h4></a>
             </div>
         </li>
         <li>
-            <img src="../images/sevaaya_samarpanam.jpg" class="img-responsive"/>
+            <img src="../highlights/2017/sevaaya_samarpanam.jpg" class="img-responsive"/>
             <span class="date">15<sup>th</sup> - 22<sup>nd</sup> March 2017</span>
             <div class="quote">
-                <a href="#"><h4>Special Lecture: Sevaaya Samarpanam<br/><small>The Department of Social Work, Organized a series of Special Lectures on 15th - 22nd March, 2017 on the topic "Sevaaya Samarpanam" for the outgoing UG Jayantians at the Mini Auditorium I and II, Kristu Jayanti College.</small></h4></a>
+                <a href="#"><h4>Special Lecture: Sevaaya<br> Samarpanam<br/><small>The Department of Social Work, Organized a series of Special Lectures on 15th - 22nd March, 2017 on the topic "Sevaaya Samarpanam" for the outgoing UG Jayantians at the Mini Auditorium I and II, Kristu Jayanti College.</small></h4></a>
+            </div>
+        </li>
+		<li>
+            <img src="../highlights/2017/national_conference_criminology.jpg" class="img-responsive"/>
+            <span class="date">17<sup>th</sup> March 2017</span>
+            <div class="quote">
+                <a href="#"><h4>National Conference on<br> Criminology and Correctional<br> Services Administration:<br> Problems and Prospects<br/><small>The Department of Social Work, Organized a series of Special Lectures..</small></h4></a>
             </div>
         </li>
         <li>
-            <img src="../images/entomophagy.jpg" class="img-responsive"/>
-            <span class="date">2 sept 2017</span>
+            <img src="../highlights/2017/sustainable_development.jpg" class="img-responsive"/>
+            <span class="date">06 March 2017</span>
             <div class="quote">
-                <a href="#"><h4>International Expert Lecture On Entomophagy <br/><small>A guest lecture was conducted by the Life Science Department of Kristu Jayanti College..
+                <a href="#"><h4>Department of Social Work <br> Expert Lecture: Sustainable<br> Development through<br> Corporate Social<br> Responsibility<br/><small>The Department of Social work organised..
                 </small></h4></a>
             </div>
         </li>
+		
+		<li>
+            <img src="../highlights/2017/sustainable_development.jpg" class="img-responsive"/>
+            <span class="date">23 Feb 2017</span>
+            <div class="quote">
+                <a href="#"><h4>Environmental Awareness<br> Day- The Kristu Jayanti Way<br/><small>Nature was respected for all its valuable resources by our ancestors. But when man's greed took over,..
+                </small></h4></a>
+            </div>
+        </li>
+		
+		<li>
+            <img src="../highlights/2017/sustainable_development.jpg" class="img-responsive"/>
+            <span class="date">25 Feb 2017</span>
+            <div class="quote">
+                <a href="#"><h4> National Seminar on Biofilm<br> Technology- Problems & <br> Prospects<br/><small>With every event that it organizes, more so in the Life sciences Department, Kristu Jayanti College never fails to use an opportunity to impress and educate..
+                </small></h4></a>
+            </div>
+        </li>
+		
+		<li>
+            <img src="../highlights/2017/sustainable_development.jpg" class="img-responsive"/>
+            <span class="date">25 Feb 2017</span>
+            <div class="quote">
+                <a href="#"><h4> Guest Lecture On The Role Of Molecular Cytogenetics In The Diagnosis Of Cancer <br/><small>On the 25th of February 2017 a guest lecture was conducted by the Department of Life Science for both the PG and UG students..
+                </small></h4></a>
+            </div>
+        </li>
+		
+		<li>
+            <img src="../highlights/2017/economia_2017.jpg" class="img-responsive"/>
+            <span class="date">02 Mar 2017</span>
+            <div class="quote">
+                <a href="#"><h4> Economia is a One Day Intra Collegiate Economics fest <br/><small>Economia is a One Day Intra Collegiate Economics fest conducted by the Department of Economics..
+                </small></h4></a>
+            </div>
+        </li>
+		
+		<li>
+            <img src="../highlights/2017/enthios_2K17.jpg" class="img-responsive"/>
+            <span class="date">21-22 Feb 2017</span>
+            <div class="quote">
+                <a href="#"><h4> "Enthios 2K17" <br/><small> Congratulations Our MCA students clinched the overalls at National Level IT Fest - "Enthios 2K17" at Jyothi Nivas College, Bangalore on 21st and 22nd Februray 2017..
+                </small></h4></a>
+            </div>
+        </li>
+		
+		<li>
+            <img src="../highlights/2017/sustainable_interventions.jpg" class="img-responsive"/>
+            <span class="date">27 Feb 2017</span>
+            <div class="quote">
+                <a href="#"><h4> Department of Social Work <br> Expert Lecture on Sustainable Interventions in Community<br> Development<br/><small> The Department of Social Work organized an expert lecture on "Sustainable Interventions in Community Development"..
+                </small></h4></a>
+            </div>
+        </li>
+		
+		<li>
+            <img src="../highlights/2017/changing_paradigms.jpg" class="img-responsive"/>
+            <span class="date">25 Feb 2017</span>
+            <div class="quote">
+                <a href="#"><h4>Department of Social Work <br> Expert Lecture on Changing<br> Paradigms in Industrial<br> Relations <br/><small> The Department of Social Work organized an expert lecture on “Changing Paradigms in Industrial Relations"..
+                </small></h4></a>
+            </div>
+        </li>
+		
+		<li>
+            <img src="../highlights/2017/changing_paradigms.jpg" class="img-responsive"/>
+            <span class="date">15 Feb 2017</span>
+            <div class="quote">
+                <a href="#"><h4>World Radio Day <br/><small>World radio day was observing by the Second Semester M A Mass Communication & Journalism students at Kristu Jayanti College P G quadrangle..
+                </small></h4></a>
+            </div>
+        </li>
+		
+		<li>
+            <img src="../highlights/2017/sargostav2017.jpg" class="img-responsive"/>
+            <span class="date">13 Feb 2017</span>
+            <div class="quote">
+                <a href="#"><h4>Sargostav 2017 PG intra<br>collegiate Cultural Fest  <br/><small>Sargotsav is the annual PG intra collegiate cultural fest organised by Kristu Jayanti College..
+                </small></h4></a>
+            </div>
+        </li>
+		
+		<li>
+            <img src="../highlights/2017/hong_kong.jpg" class="img-responsive"/>
+            <span class="date">16 Feb 2017</span>
+            <div class="quote">
+                <a href="#"><h4>Department of Social Work <br>International Lecture Series<br> 2016-17: Understanding<br> Social Withdrawal of Young<br> People in Hong Kong <br/><small>Social withdrawal currently is a prominent issue faced by migrants especially by the youth..
+                </small></h4></a>
+            </div>
+        </li>
+		
+		<li>
+            <img src="../highlights/2017/australia.jpg" class="img-responsive"/>
+            <span class="date">16 Feb 2017</span>
+            <div class="quote">
+                <a href="#"><h4>Department of Social Work <br> International Lecture Series:<br> Social Work Licensing in<br> Australia<br/><small>On the 16th February 2017, as a part of the International Lecture Series, the Department of Social Work organised..
+                </small></h4></a>
+            </div>
+        </li>
+		
+		<li>
+            <img src="../highlights/2017/child_protection.jpg" class="img-responsive"/>
+            <span class="date">06 Feb 2017</span>
+            <div class="quote">
+                <a href="#"><h4>Department of Social Work <br> International Lecture Series: Child Protection and Advocacy <br/><small>The Department of Social Work of Kristu Jayanti College as part of the International Lecture series organised an expert Lecture for the academic year 2016-17..
+                </small></h4></a>
+            </div>
+        </li>
+		
+		<li>
+            <img src="../highlights/2017/child_protection.jpg" class="img-responsive"/>
+            <span class="date">16 Feb 2017</span>
+            <div class="quote">
+                <a href="#"><h4>IEEE One Day Workshop  <br/><small>The objective of this session was to be familiar with digital convergence for smart life. The sessions focused on topics relevant to both computer science and life science applications..
+                </small></h4></a>
+            </div>
+        </li>
+		
+		<li>
+            <img src="../highlights/2017/child_protection.jpg" class="img-responsive"/>
+            <span class="date">21 Jan 2017</span>
+            <div class="quote">
+                <a href="#"><h4> 32<sup>nd</sup> edition of<br> Vichaarmanthan<br/><small>The event was titled on the theme 'Fundamental Duties'. The Honorable Justice emphasized the importance of the duties and responsibilities associated with being a citizen of India. ..
+                </small></h4></a>
+            </div>
+        </li>
+		
+		<li>
+            <img src="../highlights/2017/expert_lecture.jpg" class="img-responsive"/>
+            <span class="date">09 Jan 2017</span>
+            <div class="quote">
+                <a href="#"><h4> Department of Social Work <br> Expert Lecture on Industrial Social Work <br/><small>The Department of Social Work of Kristu Jayanti College organised an Expert Lecture on the Industrial Social Work..
+                </small></h4></a>
+            </div>
+        </li>
+		
+		<li>
+            <img src="../highlights/2017/recent_trends.jpg" class="img-responsive"/>
+            <span class="date">19 Jan 2017</span>
+            <div class="quote">
+                <a href="#"><h4> Department of Social Work <br> Expert Lecture on Recent<br> trends in Social Work<br> Education <br/><small>The Department of Social Work of Kristu Jayanti College organised an Expert lecture on Recent Trends in Social Work Education..
+                </small></h4></a>
+            </div>
+        </li>
+		
+		<li>
+            <img src="../highlights/2017/youthweek.jpg" class="img-responsive"/>
+            <span class="date">12-21 Jan 2017</span>
+            <div class="quote">
+                <a href="#"><h4> Centre for Social Activities National Youth Week <br/><small>Centre for Social Activities has organized “ National Youth Week” from 12th to 21st January 2017 to commemorate the principles..
+                </small></h4></a>
+            </div>
+        </li>
+		
+		
     </ul>
 </div>
