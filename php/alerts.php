7884a239333dcae845b3c9d918e034eb2f08b2c5
JIJO JOSEPH <jijoputhiyath@gmail.com>
2017-05-11 13:16:06 +0530

    slight - changes

--- php/alerts.php
+++ php/alerts.php
@@ -1,5 +1,5 @@
 <div class="newsAlert">
-    <marquee onmouseover="stop();"  onmouseout="start();">This is news flash.</marquee>
+    <marquee onmouseover="stop();"  onmouseout="start();">All UG Classes will commence on 20<sup>th</sup> June 2017 | All PG Classes will commence on 20<sup>th</sup> June 2017</marquee>
     <div class='hide-btn'>
         <i class="fa fa-angle-double-left"></i>
     </div>
