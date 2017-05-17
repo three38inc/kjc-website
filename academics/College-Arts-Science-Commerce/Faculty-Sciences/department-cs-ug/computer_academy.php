76385f29400b9453c43d1b43be641845667e1caf
PraveenKumarkjc <pravisweets@gmail.com>
2017-05-17 17:11:47 +0530

    pictures changes
    recent updates  under master

--- academics/College-Arts-Science-Commerce/Faculty-Sciences/department-cs-ug/computer_academy.php
+++ academics/College-Arts-Science-Commerce/Faculty-Sciences/department-cs-ug/computer_academy.php
@@ -80,6 +80,7 @@
                         <div class="row">
                             <div class="col-lg-9">
 								<div class="content-left myWelcome wow fadeIn">
+									 <img src="images/computerscience_banner.jpg" class="img-responsive" style="margin-bottom:20px;" />
 
                                 <h4 class="mainheading"><i class="fa fa-bookmark"></i>
                                     <span>&nbsp; Computer <strong> Academy  </strong></span></h4>
--- academics/College-Arts-Science-Commerce/Faculty-Sciences/department-lifescience/faculty.php
+++ academics/College-Arts-Science-Commerce/Faculty-Sciences/department-lifescience/faculty.php
@@ -79,7 +79,9 @@
 
                         <div class="row">
                             <div class="col-lg-9">
-
+								  <div class="content-left myWelcome wow fadeIn">
+								<img src="images/lifesciences_banner.jpg" class="img-responsive" style="margin-bottom:20px;" />
+ 
                                 <h4 class="mainheading"><i class="fa fa-bookmark"></i>
                                     <span>&nbsp;Faculty <strong>Profile</strong></span>
 
