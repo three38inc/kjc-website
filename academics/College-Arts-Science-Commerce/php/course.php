f1aba07363ef95798cee0a5b8ff1cabbf1cf66e9
PraveenKumarkjc <pravisweets@gmail.com>
2017-05-30 17:44:42 +0530

    changes
    recent

--- academics/College-Arts-Science-Commerce/php/course.php
+++ academics/College-Arts-Science-Commerce/php/course.php
@@ -78,7 +78,7 @@
         </div>
         
         <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 science" data-category="science">
-            <a href="Faculty-Sciences/department-cs-pg/mca.php">
+            <a href="Faculty-Sciences/department-cs-pg/index.php">
                 <div class="courseTitle color-1">
                     <i class="fa fa-laptop c1" style="float: left;"></i>
                     <h2>MCA</h2>
