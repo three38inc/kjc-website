77933a278b35821a893bb071c13ee490d871c5ca
PraveenKumarkjc <pravisweets@gmail.com>
2017-05-30 14:43:22 +0530

    mba changes
    latest updates

--- academics/School-of-Management/mba.php
+++ academics/School-of-Management/mba.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html> <?php  $ROOT="http://".$_SERVER['HTTP_HOST']."/Github/kjc-website";/*$ROOT is the base URL & should on top of every PAGE */?> 
 <html lang="en" class="no-js">
 
-    <head>
+   <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,61 +9,63 @@
     <!--<meta name="description" content="Sidebar Transitions: Transition effects for off-canvas views" />
 <meta name="keywords" content="transition, off-canvas, navigation, effect, 3d, css3, smooth" />-->
     <meta name="author" content="Three38 inc" />
-    <link rel="shortcut icon" href="../../../../images/icon.png">
+    <link rel="shortcut icon" href="../../images/icon.png">
     <!-- Bootstrap css -->
-    <link href="../../../../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <!--additional css-->
-    <link rel="stylesheet" type="text/css" href="../../../../css/component.css" />
-    <link rel="stylesheet" href="../../../../css/font-awesome.css">
-    <link rel="stylesheet" href="../../../../css/font-awesome.min.css">
-    <link rel="stylesheet" href="../../../../css/roundIcons.css">
-    <link rel="stylesheet" href="../../../../css/hoverEffect.css">
-    <link rel="stylesheet" href="../../../../css/morphing.css">
-    <link rel="stylesheet" href="../../../../css/navTabs.css">
-    <link rel="stylesheet" href="../../../../css/animate.css">
-    <link rel="stylesheet" href="../../../../css/fakeLoader.css">
+    <link rel="stylesheet" type="text/css" href="../../css/component.css" />
+    <link rel="stylesheet" href="../../css/font-awesome.css">
+    <link rel="stylesheet" href="../../css/font-awesome.min.css">
+    <link rel="stylesheet" href="../../css/roundIcons.css">
+    <link rel="stylesheet" href="../../css/hoverEffect.css">
+    <link rel="stylesheet" href="../../css/morphing.css">
+    <link rel="stylesheet" href="../../css/navTabs.css">
+    <link rel="stylesheet" href="../../css/animate.css">
+    <link rel="stylesheet" href="../../css/fakeLoader.css">
+    <link href="../../css/owl.carousel.min.css" rel="stylesheet" />
     <!--        <link rel="stylesheet" href="css/res-navtabs.css">-->
     <!--custom css-->
-    <link href="../../../../css/custom.css" rel="stylesheet">
-    <link href="../../../../css/innerpage.css" rel="stylesheet">
+    <link href="../../css/custom.css" rel="stylesheet">
+    <link href="../../css/innerpage.css" rel="stylesheet">
+    <link href="css/manSchool.css" rel="stylesheet">
     <!-- javascript -->
-    <script src="../../../../js/jquery-1.11.0.min.js"></script>
-    <script src="../../../../bootstrap/js/bootstrap.min.js"></script>
-    <script src="../../../../js/respond.js"></script>
-    <script src="../../../../js/custom.js"></script>
-    <script src="../../../../js/fakeLoader.js"></script>
+    <script src="../../js/jquery-1.11.0.min.js"></script>
+    <script src="../../bootstrap/js/bootstrap.min.js"></script>
+    <script src="../../js/respond.js"></script>
+    <script src="../../js/custom.js"></script>
+    <script src="../../js/fakeLoader.js"></script>
 
-    </head>
+</head>
 
-    <body>
-        <div id="fakeLoader"></div>
-        <script>
-            $("#fakeLoader").fakeLoader({
-                timeToHide: 2000,
-                bgColor: "#2c3e50",
-                spinner: "spinner4"
-            });
+<body>
+    <div id="fakeLoader"></div>
+    <script>
+        $("#fakeLoader").fakeLoader({
+            timeToHide: 2000,
+            bgColor: "#2c3e50",
+            spinner: "spinner4"
+        });
 
-        </script>
-        <div id="fb-root"></div>
-        <script>
-            (function(d, s, id) {
-                var js, fjs = d.getElementsByTagName(s)[0];
-                if (d.getElementById(id)) return;
-                js = d.createElement(s);
-                js.id = id;
-                js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=578768295582897";
-                fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));
+    </script>
+    <div id="fb-root"></div>
+    <script>
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=578768295582897";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
 
-        </script>
-        <div class="container-fluid">
-            <div class="row">
-                <?php require ("php/navbar.php"); ?>
-            </div>
+    </script>
+    <div class="container-fluid">
+        <div class="row">
+            <?php include ('php/navbar.php');  ?>
         </div>
-        <div id="st-container" class="st-container">
-            <?php include("../../php/offCanvasMenu.php"); ?>
+    </div>
+    <div id="st-container" class="st-container">
+        <?php include('../../php/offCanvasMenu.php'); ?>
             <!-- content push wrapper -->
             <div class="st-pusher">
                 <div class="st-content">
@@ -79,8 +81,8 @@
                             <div class="row">
                                 <div class="col-lg-9">
                                     <div class="content-left myWelcome wow fadeIn">
-                                        <img src="../../images/clg_history.jpg" class="img-responsive" style="margin-bottom:20px;" />
-                                        <br/>
+                                        <img src="images/banner.jpg" class="img-responsive" style="margin-bottom:20px;" />
+                                      
                                         <h4 class="mainheading"><i class="fa fa-bookmark"></i>
                                             <span>&nbsp; Welcome <strong>PGDM &amp; MBA</strong></span></h4>
 
@@ -204,20 +206,20 @@
             })();
 
         </script>
-         <script src="../../../../js/modernizr.custom.js"></script>
-    <script src="../../../../js/spin.min.js"></script>
-    <script src="../../../../js/mySpin.js"></script>
-    <script src="../../../../js/swipeview.js"></script>
-    <script src="../../../../js/jquery.bootstrap.newsbox.min.js"></script>
-    <script src="../../../../js/classie.js"></script>
-    <script src="../../../../js/morphingSearch.js"></script>
-    <script src="../../../../js/sidebarEffects.js"></script>
-    <script src="../../../../js/transit.js"></script>
-    <script src="../../../../js/sly.js"></script>
-    <script src="../../../../js/customSly.js"></script>
-    <script src="../../../../js/wow.min.js"></script>
+          <script src="../../js/modernizr.custom.js"></script>
+    <script src="../../js/spin.min.js"></script>
+    <script src="../../js/mySpin.js"></script>
+    <script src="../../js/swipeview.js"></script>
+    <script src="../../js/jquery.bootstrap.newsbox.min.js"></script>
+    <script src="../../js/classie.js"></script>
+    <script src="../../js/morphingSearch.js"></script>
+    <script src="../../js/sidebarEffects.js"></script>
+    <script src="../../js/transit.js"></script>
+    <script src="../../js/sly.js"></script>
+    <script src="../../js/customSly.js"></script>
+    <script src="../../js/wow.min.js"></script>
     <!--    <script src="js/res-navtabs.js"></script>-->
-    <script src="../../../../js/scripts.js"></script>
+    <script src="../../js/scripts.js"></script>
         <script type="text/javascript">
             function googleTranslateElementInit() {
                 new google.translate.TranslateElement({
