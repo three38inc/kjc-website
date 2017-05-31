aebe9a9a2a74c63d7fee1554df92fe40f935df0c
PraveenKumarkjc <pravisweets@gmail.com>
2017-05-31 12:23:06 +0530

    default banner
    recent updates

--- about/founder.php
+++ about/founder.php
@@ -110,7 +110,7 @@
                             <div class="marginTopDiv">&nbsp;</div>
                             <div class="col-lg-9">
                                 <div class="content-left myWelcome wow fadeIn">
-                                    <img src="../images/clg_history.jpg" class="img-responsive" style="margin-bottom:20px;" />
+                                    <img src="../images/founder.jpg" class="img-responsive" style="margin-bottom:20px;" />
                                 
                                     <h4 class="mainheading"><i class="fa fa-bookmark"></i>
                                         <span>&nbsp;Our <strong>Founder</strong></span>
