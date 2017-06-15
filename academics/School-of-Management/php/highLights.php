36cd5231f59a059a1cd0bad6d7ae6f53f0a1af47
PraveenKumarkjc <pravisweets@gmail.com>
2017-06-15 14:07:02 +0530

    highlights
    recent changes

--- academics/School-of-Management/php/highLights.php
+++ academics/School-of-Management/php/highLights.php
@@ -9,55 +9,22 @@
     <ul id="frameHighLights" class="owl-carousel">
         <li>
             <img src="../../highlights/2017/socialwork_camp_2017.jpg" class="img-responsive"/>
-            <span class="date">30<sup>th</sup> March 2017</span>
+            <span class="date">16<sup>th</sup> Jan 2016</span>
             <div class="quote">
-                <a href="../../highlights/social_work_camp_2017.php"><h4>Social Work Camp 2017<br/><small>
-				The Social Work Camp organized by the Department of Social Work, Kristu Jayanti College, Bangalore..</small></h4></a>
+                <a href="../../highlights/union_budget.php"><h4>Panel discussion on UNION BUDGET 2016-2017<br/><small>
+				The program started at 2.00 pm in SKE auditorium. Dr. B.L Bhagyalakshmi, Former Director, Dept. of college of education, Govt of Karnataka..</small></h4></a>
             </div>
         </li>
         <li>
             <img src="../../highlights/2017/socialwork_camp_2017.jpg" class="img-responsive"/>
             <span class="date">20<sup>th</sup> March 2017</span>
             <div class="quote">
-              <a href="../../highlights/fdp_data_analysis_natural_sciences.php"><h4>FDP on Data Analysis In<br> Natural Sciences<br/><small>The Department of Life Sciences conducted Faculty Development Programme on Data Analysis..</small></h4></a>
+              <a href="../../highlights/3i_program.php"><h4>Industry Institute Interface Series <br/><small>3i Program is one of the series conducted in Kristu Jayanti School of Management where various Managers and Leaders from IT Industry..</small></h4></a>
             </div>
         </li>
-        <li>
-            <img src="../../highlights/2017/sensitivity.jpg" class="img-responsive"/>
-            <span class="date">24 - 25 March 2017</span>
-            <div class="quote">
-                <a href="../../highlights/workshop_gender_sensitivity"><h4>Workshop on Gender<br>  Sensitivity<br/><small>The Department of Social Work organized a two day workshop on Gender Sensitivity"..</small></h4></a>
-            </div>
-        </li>
-        <li>
-            <img src="../../highlights/2017/socail_work_day.jpg" class="img-responsive"/>
-            <span class="date">24<sup>th</sup> March 2017</span>
-            <div class="quote">
-                <a href="../../highlights/social_workers_day.php"><h4>World Social Worker's Day<br> Celebration<br/><small>The Department of Social Work organized a Program followed by an exhibition on account of World Social Workers Day on the  in the college premises.</small></h4></a>
-            </div>
-        </li>
-        <li>
-            <img src="../../highlights/2017/sevaaya_samarpanam.jpg" class="img-responsive"/>
-            <span class="date">15<sup>th</sup> - 22<sup>nd</sup> March 2017</span>
-            <div class="quote">
-                <a href="../../highlights/sevaaya_samarpanam.php"><h4>Special Lecture: Sevaaya<br> Samarpanam<br/><small>The Department of Social Work, Organized a series of Special Lectures on 15th - 22nd March, 2017 on the topic "Sevaaya Samarpanam" for the outgoing UG Jayantians at the Mini Auditorium I and II, Kristu Jayanti College.</small></h4></a>
-            </div>
-        </li>
-		<li>
-            <img src="../../highlights/2017/national_conference_criminology.jpg" class="img-responsive"/>
-            <span class="date">17<sup>th</sup> March 2017</span>
-            <div class="quote">
-                <a href="../../highlights/criminology.php"><h4>National Conference on<br> Criminology and Correctional<br> Services Administration:<br> Problems and Prospects<br/><small>The Department of Social Work, Organized a series of Special Lectures..</small></h4></a>
-            </div>
-        </li>
-        <li>
-            <img src="../../highlights/2017/sustainable_development.jpg" class="img-responsive"/>
-            <span class="date">06 March 2017</span>
-            <div class="quote">
-                <a href="../../highlights/corporate_social_responsibility.php"><h4>Department of Social Work <br> Expert Lecture: Sustainable<br> Development through<br> Corporate Social<br> Responsibility<br/><small>The Department of Social work organised..
-                </small></h4></a>
-            </div>
-        </li>
+        
+       
+    
 		
 		<li>
             <img src="../../highlights/2017/sustainable_development.jpg" class="img-responsive"/>
@@ -68,68 +35,10 @@
             </div>
         </li>
 		
-		<li>
-            <img src="../../highlights/2017/sustainable_development.jpg" class="img-responsive"/>
-            <span class="date">25 Feb 2017</span>
-            <div class="quote">
-                <a href="../../highlights/biofilm_technology.php"><h4> National Seminar on Biofilm<br> Technology- Problems & <br> Prospects<br/><small>With every event that it organizes, more so in the Life sciences Department, Kristu Jayanti College never fails to use an opportunity to impress and educate..
-                </small></h4></a>
-            </div>
-        </li>
+	
+	
 		
-		<li>
-            <img src="../../highlights/2017/sustainable_development.jpg" class="img-responsive"/>
-            <span class="date">25 Feb 2017</span>
-            <div class="quote">
-                <a href="../../highlights/molecular_cytogenetics.php"><h4> Guest Lecture On The Role Of Molecular Cytogenetics In The Diagnosis Of Cancer <br/><small>On the 25th of February 2017 a guest lecture was conducted by the Department of Life Science for both the PG and UG students..
-                </small></h4></a>
-            </div>
-        </li>
-		
-		<li>
-            <img src="../../highlights/2017/economia_2017.jpg" class="img-responsive"/>
-            <span class="date">02 Mar 2017</span>
-            <div class="quote">
-                <a href="../../highlights/economia_2017.php"><h4> Economia is a One Day Intra Collegiate Economics fest <br/><small>Economia is a One Day Intra Collegiate Economics fest conducted by the Department of Economics..
-                </small></h4></a>
-            </div>
-        </li>
-		
-		<li>
-            <img src="../../highlights/2017/enthios_2K17.jpg" class="img-responsive"/>
-            <span class="date">21-22 Feb 2017</span>
-            <div class="quote">
-                <a href="../../highlights/enthios 2K17.php"><h4> "Enthios 2K17" <br/><small> Congratulations Our MCA students clinched the overalls at National Level IT Fest - "Enthios 2K17" at Jyothi Nivas College, Bangalore on 21st and 22nd Februray 2017..
-                </small></h4></a>
-            </div>
-        </li>
-		
-		<li>
-            <img src="../../highlights/2017/sustainable_interventions.jpg" class="img-responsive"/>
-            <span class="date">27 Feb 2017</span>
-            <div class="quote">
-                <a href="../../highlights/sustainable_interventions.php"><h4> Department of Social Work <br> Expert Lecture on Sustainable Interventions in Community<br> Development<br/><small> The Department of Social Work organized an expert lecture on "Sustainable Interventions in Community Development"..
-                </small></h4></a>
-            </div>
-        </li>
-		
-		<li>
-            <img src="../../highlights/2017/changing_paradigms.jpg" class="img-responsive"/>
-            <span class="date">25 Feb 2017</span>
-            <div class="quote">
-                <a href="../../highlights/changing_paradigms.php"><h4>Department of Social Work <br> Expert Lecture on Changing<br> Paradigms in Industrial<br> Relations <br/><small> The Department of Social Work organized an expert lecture on “Changing Paradigms in Industrial Relations"..
-                </small></h4></a>
-            </div>
-        </li>
-		
-		<li>
-            <img src="../../highlights/2017/world_radio_day.jpg" class="img-responsive"/>
-            <span class="date">15 Feb 2017</span>
-            <div class="quote">
-                <a href="../../highlights/world_radio_day.php"><h4>World Radio Day <br/><small>World radio day was observing by the Second Semester M A Mass Communication & Journalism students at Kristu Jayanti College P G quadrangle..
-                </small></h4></a>
-            </div>
-        </li>
+	
 		
 		<li>
             <img src="../../highlights/2017/sargostav2017.jpg" class="img-responsive"/>
@@ -140,41 +49,10 @@
             </div>
         </li>
 		
-		<li>
-            <img src="../../highlights/2017/hong_kong.jpg" class="img-responsive"/>
-            <span class="date">16 Feb 2017</span>
-            <div class="quote">
-                <a href="../../highlights/ils_hongkong.php"><h4>Department of Social Work <br>International Lecture Series<br> 2016-17: Understanding<br> Social Withdrawal of Young<br> People in Hong Kong <br/><small>Social withdrawal currently is a prominent issue faced by migrants especially by the youth..
-                </small></h4></a>
-            </div>
-        </li>
 		
-		<li>
-            <img src="../../highlights/2017/australia.jpg" class="img-responsive"/>
-            <span class="date">16 Feb 2017</span>
-            <div class="quote">
-                <a href="../../highlights/ils_australia.php"><h4>Department of Social Work <br> International Lecture Series:<br> Social Work Licensing in<br> Australia<br/><small>On the 16th February 2017, as a part of the International Lecture Series, the Department of Social Work organised..
-                </small></h4></a>
-            </div>
-        </li>
 		
-		<li>
-            <img src="../../highlights/2017/child_protection.jpg" class="img-responsive"/>
-            <span class="date">06 Feb 2017</span>
-            <div class="quote">
-                <a href="../../highlights/child_protection.php"><h4>Department of Social Work <br> International Lecture Series: Child Protection and Advocacy <br/><small>The Department of Social Work of Kristu Jayanti College as part of the International Lecture series organised an expert Lecture for the academic year 2016-17..
-                </small></h4></a>
-            </div>
-        </li>
 		
-		<li>
-            <img src="../../highlights/2017/child_protection.jpg" class="img-responsive"/>
-            <span class="date">16 Feb 2017</span>
-            <div class="quote">
-                <a href="../../highlights/ieee_workshop.php"><h4>IEEE One Day Workshop  <br/><small>The objective of this session was to be familiar with digital convergence for smart life. The sessions focused on topics relevant to both computer science and life science applications..
-                </small></h4></a>
-            </div>
-        </li>
+		
 		
 		<li>
             <img src="../../highlights/2017/child_protection.jpg" class="img-responsive"/>
@@ -184,24 +62,9 @@
                 </small></h4></a>
             </div>
         </li>
+	
 		
-		<li>
-            <img src="../../highlights/2017/expert_lecture.jpg" class="img-responsive"/>
-            <span class="date">09 Jan 2017</span>
-            <div class="quote">
-                <a href="../../highlights/expert_lecture_industrial.php"><h4> Department of Social Work <br> Expert Lecture on Industrial Social Work <br/><small>The Department of Social Work of Kristu Jayanti College organised an Expert Lecture on the Industrial Social Work..
-                </small></h4></a>
-            </div>
-        </li>
 		
-		<li>
-            <img src="../../highlights/2017/recent_trends.jpg" class="img-responsive"/>
-            <span class="date">19 Jan 2017</span>
-            <div class="quote">
-                <a href="../../highlights/expert_lecture_recent.php"><h4> Department of Social Work <br> Expert Lecture on Recent<br> trends in Social Work<br> Education <br/><small>The Department of Social Work of Kristu Jayanti College organised an Expert lecture on Recent Trends in Social Work Education..
-                </small></h4></a>
-            </div>
-        </li>
 		
 		<li>
             <img src="../../highlights/2017/youthweek.jpg" class="img-responsive"/>
---
+++ highlights/3i_program.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html> <?php  $ROOT="http://".$_SERVER['HTTP_HOST']."/Github/kjc-website";/*$ROOT is the base URL & should on top of every PAGE */?> 
+<html lang="en" class="no-js">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kristu Jayanti College, Bengaluru</title>
+    <!--<meta name="description" content="Sidebar Transitions: Transition effects for off-canvas views" />
+<meta name="keywords" content="transition, off-canvas, navigation, effect, 3d, css3, smooth" />-->
+    <meta name="author" content="Three38 inc" />
+    <link rel="shortcut icon" href="../images/icon.png">
+    <!-- Bootstrap css -->
+    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!--additional css-->
+    <link rel="stylesheet" type="text/css" href="../css/component.css" />
+    <link rel="stylesheet" href="../css/font-awesome.css">
+    <link rel="stylesheet" href="../css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/roundIcons.css">
+    <link rel="stylesheet" href="../css/hoverEffect.css">
+    <link rel="stylesheet" href="../css/morphing.css">
+    <link rel="stylesheet" href="../css/navTabs.css">
+    <link rel="stylesheet" href="../css/animate.css">
+    <link rel="stylesheet" href="../css/fakeLoader.css">
+    <!--        <link rel="stylesheet" href="css/res-navtabs.css">-->
+    <!--custom css-->
+    <link href="../css/custom.css" rel="stylesheet">
+    <link href="../css/innerpage.css" rel="stylesheet">
+    <!-- javascript -->
+    <script src="../js/jquery-1.11.0.min.js"></script>
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
+    <script src="../js/respond.js"></script>
+    <script src="../js/custom.js"></script>
+    <script src="../js/fakeLoader.js"></script>
+    <style>
+        .link-arrow a.a-link-arrow::before {
+            left: 0px;
+            z-index: -1;
+            width: 100%;
+            background: #f3f3f3;
+        }
+        
+        .link-arrow {
+            /* background: rgba(149,165,166,0.4); */
+            background: rgba(255, 255, 255, 0);
+        }
+        
+        .link-arrow a.a-link-arrow {
+            left: 10px;
+        }
+        
+        .menu-list ul li {
+            background: transparent;
+        }
+        
+        .side-menu {
+            background: transparent;
+        }
+        
+        .content-right {
+            background: transparent;
+            box-shadow: none;
+        }
+
+    </style>
+</head>
+
+<body>
+    <script type="text/javascript">$crisp=[];CRISP_WEBSITE_ID="bfee4615-2d13-4129-a9c2-d7b8095e3d0e";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.im/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
+    <div id="fakeLoader"></div>
+    <script>
+        $("#fakeLoader").fakeLoader({
+            timeToHide: 2000,
+            bgColor: "#2c3e50",
+            spinner: "spinner4"
+        });
+
+    </script>
+    <div id="fb-root"></div>
+    <script>
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=578768295582897";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+
+    </script>
+    <div class="container-fluid">
+        <div class="row">
+            <?php include ('../php/navbar.php');  ?>
+        </div>
+    </div>
+    <div id="st-container" class="st-container">
+        <?php include('../php/offCanvasMenu.php'); ?>
+        <!-- content push wrapper -->
+        <div class="st-pusher">
+            <div class="st-content">
+                <!-- this is the wrapper for the content -->
+                <div class="st-content-inner">
+                    <div class="container">
+                        <!--        <div class="row">-->
+                        <div class="row" style="margin-top: 100px; padding-left: 10px; padding-right: 10px;">
+
+                        </div>
+                        <div class="row">
+                            <div class="marginTopDiv">&nbsp;</div>
+                            <div class="col-lg-9">
+                                <div class="content-left myWelcome wow fadeIn">
+                                   
+                                    
+                                    <h4 class="mainheading"><i class="fa fa-bookmark"></i>
+                                        <span>&nbsp; Industry Institute  <strong>Interface Series </strong></span></h4>
+
+									<p align=justify>3i Program is one of the series conducted in Kristu Jayanti School of Management where various Managers and Leaders from IT Industry spend their time with the students sharing their experiences and values which enables the students to focus more and to adapt to every change.</p>
+
+									<p align=justify>The School of Management, had organized the Industry Institute Interface Series for the students of II Trimester MBA&PGDM on 16th January 2016 at 11am to inculcate and train the students on developing the ability to perceive. The program commenced by invoking the presence of the almighty God. During the session, the students were given brief knowledge about different Theories and of making quality work and being successful leaders. Mr. Arvind Warrier was invited to take over the Session. His life experiences and examples on his personal and Professional life did bring an entire different outlook on our perspective and brought about a transforming session on being leaders. In Address to the students he shared the certain key important views of developing the habit of updating our personal profile every two weeks and also shared the importance of reading and documentation.</p>
+
+									<p align=justify>According to Mr. Arvind Warrier it is very important that each time we need to measure our life, cost, time and quantity”. Mr. Arvind Warrier encouraged the students to focus on learning, Unlearning and Relearning and emphasized on building the Blue Ocean Strategy.</p>
+
+									<p align=justify>At the end of the session, the students were given time to discuss and to clear all their doubts on the subject. Dr. S. Arul Senthilkumar facilitated by giving away a memento of appreciation to Mr. Arvind Warrier for his efforts for taking time to speak to the students of Kristu Jayanti School of Management.</p>
+
+
+									   
+
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                               <div class="content-right wow fadeIn" style="background-color: transparent;box-shadow: none;">
+                                   
+
+
+                               
+                               
+                               <iframe src="../events/right_events.php" scrolling="no" height="1000" style="border:0;"></iframe> 
+
+
+                                  
+
+
+                            </div>
+                        </div>
+
+                    </div>
+
+                       </div>
+                    <?php include ('../php/alerts.php');  ?>
+                    <?php include('../php/footer.php'); ?>
+                    <?php include('../php/morphingSearch.php'); ?>
+                </div>
+
+
+
+            </div>
+            <!--/st-content-inner-->
+        </div>
+        <!-- this is the wrapper for the content-->
+    </div>
+    <!--st-pusher-->
+
+    <!--st-container-->
+
+    <script src="../js/cbpFWTabs.js"></script>
+    <script>
+        (function() {
+
+            [].slice.call(document.querySelectorAll('.tabs')).forEach(function(el) {
+                new CBPFWTabs(el);
+            });
+
+        })();
+
+    </script>
+    <script src="../js/modernizr.custom.js"></script>
+    <script src="../js/spin.min.js"></script>
+    <script src="../js/mySpin.js"></script>
+    <script src="../js/swipeview.js"></script>
+    <script src="../js/jquery.bootstrap.newsbox.min.js"></script>
+    <script src="../js/classie.js"></script>
+    <script src="../js/morphingSearch.js"></script>
+    <script src="../js/sidebarEffects.js"></script>
+    <script src="../js/transit.js"></script>
+    <script src="../js/sly.js"></script>
+    <script src="../js/customSly.js"></script>
+    <script src="../js/wow.min.js"></script>
+    <!--    <script src="js/res-navtabs.js"></script>-->
+    <script src="../js/scripts.js"></script>
+    <script type="text/javascript">
+        function googleTranslateElementInit() {
+            new google.translate.TranslateElement({
+                pageLanguage: 'en'
+            }, 'google_translate_element');
+        }
+
+    </script>
+    <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+</body>
+
+</html>
