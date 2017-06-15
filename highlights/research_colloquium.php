50955392c7edc9576b93d40c9f13d33a41030298
PraveenKumarkjc <pravisweets@gmail.com>
2017-06-15 12:46:00 +0530

    mca highlights
    recent updates

---
+++ highlights/research_colloquium.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html> <?php  $ROOT="http://".$_SERVER['HTTP_HOST']."/Github/kjc-website";/*$ROOT is the base URL & should on top of every PAGE */?> 
+<html lang="en" class="no-js">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kristu Jayanti College, Bengaluru</title>
+    <!--<meta name="description" content="Sidebar Transitions: Transition effects for off-canvas views" />
+<meta name="keywords" content="transition, off-canvas, navigation, effect, 3d, css3, smooth" />-->
+    <meta name="author" content="Three38 inc" />
+    <link rel="shortcut icon" href="../images/icon.png">
+    <!-- Bootstrap css -->
+    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!--additional css-->
+    <link rel="stylesheet" type="text/css" href="../css/component.css" />
+    <link rel="stylesheet" href="../css/font-awesome.css">
+    <link rel="stylesheet" href="../css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/roundIcons.css">
+    <link rel="stylesheet" href="../css/hoverEffect.css">
+    <link rel="stylesheet" href="../css/morphing.css">
+    <link rel="stylesheet" href="../css/navTabs.css">
+    <link rel="stylesheet" href="../css/animate.css">
+    <link rel="stylesheet" href="../css/fakeLoader.css">
+    <!--        <link rel="stylesheet" href="css/res-navtabs.css">-->
+    <!--custom css-->
+    <link href="../css/custom.css" rel="stylesheet">
+    <link href="../css/innerpage.css" rel="stylesheet">
+    <!-- javascript -->
+    <script src="../js/jquery-1.11.0.min.js"></script>
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
+    <script src="../js/respond.js"></script>
+    <script src="../js/custom.js"></script>
+    <script src="../js/fakeLoader.js"></script>
+    <style>
+        .link-arrow a.a-link-arrow::before {
+            left: 0px;
+            z-index: -1;
+            width: 100%;
+            background: #f3f3f3;
+        }
+        
+        .link-arrow {
+            /* background: rgba(149,165,166,0.4); */
+            background: rgba(255, 255, 255, 0);
+        }
+        
+        .link-arrow a.a-link-arrow {
+            left: 10px;
+        }
+        
+        .menu-list ul li {
+            background: transparent;
+        }
+        
+        .side-menu {
+            background: transparent;
+        }
+        
+        .content-right {
+            background: transparent;
+            box-shadow: none;
+        }
+
+    </style>
+</head>
+
+<body>
+    <script type="text/javascript">$crisp=[];CRISP_WEBSITE_ID="bfee4615-2d13-4129-a9c2-d7b8095e3d0e";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.im/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
+    <div id="fakeLoader"></div>
+    <script>
+        $("#fakeLoader").fakeLoader({
+            timeToHide: 2000,
+            bgColor: "#2c3e50",
+            spinner: "spinner4"
+        });
+
+    </script>
+    <div id="fb-root"></div>
+    <script>
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=578768295582897";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+
+    </script>
+    <div class="container-fluid">
+        <div class="row">
+            <?php include ('../php/navbar.php');  ?>
+        </div>
+    </div>
+    <div id="st-container" class="st-container">
+        <?php include('../php/offCanvasMenu.php'); ?>
+        <!-- content push wrapper -->
+        <div class="st-pusher">
+            <div class="st-content">
+                <!-- this is the wrapper for the content -->
+                <div class="st-content-inner">
+                    <div class="container">
+                        <!--        <div class="row">-->
+                        <div class="row" style="margin-top: 100px; padding-left: 10px; padding-right: 10px;">
+
+                        </div>
+                        <div class="row">
+                            <div class="marginTopDiv">&nbsp;</div>
+                            <div class="col-lg-9">
+                                <div class="content-left myWelcome wow fadeIn">
+                                   
+                                    
+                                    <h4 class="mainheading"><i class="fa fa-bookmark"></i>
+                                        <span>&nbsp; Organizing Research <strong> Colloquium </strong></span></h4>
+
+									<p><b>Objective:</b><br>
+• To enhance the quality of Research articles from the science deanery<br>
+• To create an awareness about how to write project proposals<br>
+										• Awareness about various funding agencies</p>
+
+									<p><b>Resource person:</b><br>
+										Dr. Subashini. P, Professor, Department of Computer Science, Avinashilingam Institute for Home Science and Higher Education for Women, Coimbatore</p>
+
+									<p>Research colloquium is a programme organized by the department of Computer which is conducted once in every semester since 2011. This is the eleventh session of research colloquium. This session which is on- How to prepare a research proposal and which are the funding agencies and how to identify the quality of journals that we choose for publications</p>
+
+									<p>The first part of the session dealt with the W5H principles- Why, Who, What, Where, When and How to proceed with Projects. She presented the model of writing a project proposal including what all aspects to take care while preparing Budget</p>
+
+									<p>Second part of the session was information on the various funding agencies, and their probable-“Call for Proposal’ time.</p>
+
+									<p>The third part gave us insights on how to identify a journal is indexed by approved agencies or not. She mentioned the currentl7y accepted indices as – Scopus (Reuters), SCI (Elsevier) and Global Impact factors elaborately.</p>
+
+									<p>The session was very informative and inspiring. She also offered to assistance and support to faculty members in preparing of proposals and writing of quality research articles.									   </p>
+
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                               <div class="content-right wow fadeIn" style="background-color: transparent;box-shadow: none;">
+                                   
+
+
+                               
+                               
+                              <iframe src="right_frame.php" scrolling="no" height="1000" style="border:0;"></iframe> 
+
+
+                                  
+
+
+                            </div>
+                        </div>
+
+                    </div>
+
+                       </div>
+                    <?php include ('../php/alerts.php');  ?>
+                    <?php include('../php/footer.php'); ?>
+                    <?php include('../php/morphingSearch.php'); ?>
+                </div>
+
+
+
+            </div>
+            <!--/st-content-inner-->
+        </div>
+        <!-- this is the wrapper for the content-->
+    </div>
+    <!--st-pusher-->
+
+    <!--st-container-->
+
+    <script src="../js/cbpFWTabs.js"></script>
+    <script>
+        (function() {
+
+            [].slice.call(document.querySelectorAll('.tabs')).forEach(function(el) {
+                new CBPFWTabs(el);
+            });
+
+        })();
+
+    </script>
+    <script src="../js/modernizr.custom.js"></script>
+    <script src="../js/spin.min.js"></script>
+    <script src="../js/mySpin.js"></script>
+    <script src="../js/swipeview.js"></script>
+    <script src="../js/jquery.bootstrap.newsbox.min.js"></script>
+    <script src="../js/classie.js"></script>
+    <script src="../js/morphingSearch.js"></script>
+    <script src="../js/sidebarEffects.js"></script>
+    <script src="../js/transit.js"></script>
+    <script src="../js/sly.js"></script>
+    <script src="../js/customSly.js"></script>
+    <script src="../js/wow.min.js"></script>
+    <!--    <script src="js/res-navtabs.js"></script>-->
+    <script src="../js/scripts.js"></script>
+    <script type="text/javascript">
+        function googleTranslateElementInit() {
+            new google.translate.TranslateElement({
+                pageLanguage: 'en'
+            }, 'google_translate_element');
+        }
+
+    </script>
+    <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+</body>
+
+</html>
