e8b839daa25c33ceb9f893f5aaf02c73dbd98dd1
PraveenKumarkjc <pravisweets@gmail.com>
2017-06-16 11:15:56 +0530

    lifescience faculty profile
    recent changes

--- academics/College-Arts-Science-Commerce/Faculty-Sciences/department-lifescience/snipets/page2.php
+++ academics/College-Arts-Science-Commerce/Faculty-Sciences/department-lifescience/snipets/page2.php
@@ -89,34 +89,6 @@
             </div>
         </div>
     </div>
-    <div class="member-item col-md-4 ">
-        <div class="member-item-inner">
-            <div class="item-thumbnail">
-                <div class="class-img"><img src="images/fr_Augustine.png"></div>
-            </div>
-            <div class="item-content">
-                <h4>Dr. Jaishanker Pillai H.P.
-                </h4>
-                <p>
-                    <span>Qualification </span> <br/>M.Sc.,M.Phil., B.Ed.,Ph.D.,FBSS 
-                </p>
-                 <p>
-                    <span>Specialization </span> <br/>Microbiology 
-                </p>
-                 <p>
-                    <span>Years of experience </span> <br/>10 Years 
-                </p>
-                <p>
-                    <span>Area of Research </span><br/>Environmental &amp; Agricultural Microbiology
-                </p>
-                <ul class="list-inline social-light ">
-                    <li>
-                        <a class=" social-icon" href="mailto:#"> <i class="fa fa-envelope"></i></a>
-                    </li>
-                    <li><a class=" social-icon" href="#"><i class="fa fa-twitter"></i></a></li>
-                    <li><a class=" social-icon" href="#"><i class="fa fa-linkedin"></i></a></li>
-                </ul>
-            </div>
-        </div>
+   
     </div>
     
