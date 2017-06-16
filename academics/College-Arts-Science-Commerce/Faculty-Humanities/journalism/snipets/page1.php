c6f518d6bc4a02b700da552dc6875d89418e1018
PraveenKumarkjc <pravisweets@gmail.com>
2017-06-16 12:10:36 +0530

    journalism faculty profile
    recent updates

--- academics/College-Arts-Science-Commerce/Faculty-Humanities/journalism/snipets/page1.php
+++ academics/College-Arts-Science-Commerce/Faculty-Humanities/journalism/snipets/page1.php
@@ -10,7 +10,7 @@
                  <p>
                     <span>Designation </span> <br/>Dean,Faculty of Humanties &amp; Head of the Department 
                 </p>
-                <p>
+                <p align=justify>
                    Dr. Gopakumar A.V has around fourteen years of teaching experience spanning Kristu Jayanti College, Manipal University and Sikkim Manipal University. He started his media career with newmedia, and entered into magazine journalism as the correspondent and later as the Magazine Editor of an international health magazine "Internaturalhealth".
                 </p>
                 <ul class="list-inline social-light ">
@@ -31,7 +31,7 @@
             <div class="item-content">
                 <h4>Prof. Abraham
                 </h4>
-                 <p>
+                 <p align=justify>
                     Prof. Abraham has 21 years of teaching experience spanning Kristu Jayanti College, Baldwin Women's Methodist College, NMKRV College for Women. He also has been a Guest Faculty for UGC sponsored career oriented "add on Course" in Journalism at Government First Grade College, Vijayanagar, Bangalore
                 </p>
                 <ul class="list-inline social-light ">
@@ -40,7 +40,7 @@
                     </li>
                     <li><a class=" social-icon" href="#"><i class="fa fa-twitter"></i></a></li>
                     <li><a class=" social-icon" href="#"><i class="fa fa-linkedin"></i></a></li>
-                </ul>
+                </ul><br><br>
             </div>
         </div>
     </div>
@@ -52,7 +52,7 @@
             <div class="item-content">
                 <h4>Prof. Aby Augustine
                 </h4>
-                 <p>
+                 <p align=justify>
                   Aby Augustine alumnus of Kristu Jayanti College is a First Rank holder in Mass Communication and Journalism from Mahatma Gandhi University, Kottayam, Kerala. Began his career as a content coordinator, layout and graphic designer in Jeevadooth- A General Interest Magazine run by Diocese of Kanjirapally, Kottayam, Kerala; has also compiled, edited and published Your Speeches (Vol 1 and 2) - ISBN no: 93-801334-4-8. 
                 </p>
                 <ul class="list-inline social-light ">
@@ -61,7 +61,7 @@
                     </li>
                     <li><a class=" social-icon" href="#"><i class="fa fa-twitter"></i></a></li>
                     <li><a class=" social-icon" href="#"><i class="fa fa-linkedin"></i></a></li>
-                </ul>
+                </ul><br><br>
             </div>
         </div>
     </div>
@@ -73,7 +73,7 @@
             <div class="item-content">
                 <h4>Prof. Praveen Abraham
                 </h4>
-                 <p>
+                 <p align=justify>
                  Prof. Praveen Abraham has earned Master's Degree in Journalism from DonBosco College, Kannur and around six years of teaching experience spanning Kristu Jayanti College, De Paul Institute of Science and Technology, Angamali and Univeristy of Asmara .His areas of specialization are in television news production, magazine reporting, and communication theories.
                 </p>
                 <ul class="list-inline social-light ">
@@ -94,7 +94,7 @@
             <div class="item-content">
                 <h4>Prof. Juby Thomas
                 </h4>
-                 <p>
+                 <p align=justify>
                 Juby Thomas has 5 years of experience and an industry exposure across diverse media platforms like Journalism, Photojournalism and Public Relations. She holds the credit for numerous articles published nationally and internationally in Newspaper, Magazine and Online Media. Also published 7 research articles in reputed journals and attended several national and international seminars where she presented 11 research papers and won 2 best paper awards. She organized a UGC sponsored National level seminar in the year 2015 and completed a UGC minor research project in the year 2016. She is also the BOE and BOS member of 5 prestigious institutions in India and is a Member of Editorial board of International Journal of Mass Communication, journalism and politics.
                 </p>
                 <ul class="list-inline social-light ">
