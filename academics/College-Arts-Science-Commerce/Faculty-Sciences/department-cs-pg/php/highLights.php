35e7cf49b8a7949824ba20f844faaca465c2cd84
JIJO JOSEPH <jijoputhiyath@gmail.com>
2017-06-16 11:43:12 +0530

    slight changes

--- academics/College-Arts-Science-Commerce/Faculty-Sciences/department-cs-pg/php/highLights.php
+++ academics/College-Arts-Science-Commerce/Faculty-Sciences/department-cs-pg/php/highLights.php
@@ -9,33 +9,7 @@
     <ul id="frameHighLights" class="owl-carousel">
         
         
-       
      
-       
-		
-        
-		
-		<li>
-            <img src="../../../../highlights/2017/sustainable_development.jpg" class="img-responsive"/>
-            <span class="date">23 Feb 2017</span>
-            <div class="quote">
-                <a href="../../../../highlights/environmental_awareness.php"><h4>Environmental Awareness<br> Day- The Kristu Jayanti Way<br/><small>Nature was respected for all its valuable resources by our ancestors. But when man's greed took over,..
-                </small></h4></a>
-            </div>
-        </li>
-		
-		
-		
-		
-		<li>
-            <img src="../../../../highlights/2017/economia_2017.jpg" class="img-responsive"/>
-            <span class="date">02 Mar 2017</span>
-            <div class="quote">
-                <a href="../../../../highlights/economia_2017.php"><h4> Economia is a One Day Intra Collegiate Economics fest <br/><small>Economia is a One Day Intra Collegiate Economics fest conducted by the Department of Economics..
-                </small></h4></a>
-            </div>
-        </li>
-		
 		<li>
             <img src="../../../../highlights/2017/enthios_2K17.jpg" class="img-responsive"/>
             <span class="date">21-22 Feb 2017</span>
@@ -46,42 +20,18 @@
         </li>
 		
 		
-		
-		
-		
-		<li>
+		<!--<li>
             <img src="../../../../highlights/2017/world_radio_day.jpg" class="img-responsive"/>
             <span class="date">15 Feb 2017</span>
             <div class="quote">
-                <a href="../../../../highlights/world_radio_day.php"><h4>World Radio Day <br/><small>World radio day was observing by the Second Semester M A Mass Communication & Journalism students at Kristu Jayanti College P G quadrangle..
+                <a href="../../../../highlights/epitome_2K17.php"><h4>National Level IT Fest <br> "Epitome 2K17"<br/><small>Congratulations Our MCA students won prizes at National Level IT Fest - "Epitome 2K17" at St. Aloysius Institute of Management and Information Technology on 16th and 17th Februray 2017..
                 </small></h4></a>
             </div>
-        </li>
-		
-		<li>
-            <img src="../../../../highlights/2017/sargostav2017.jpg" class="img-responsive"/>
-            <span class="date">13 Feb 2017</span>
-            <div class="quote">
-                <a href="../../../../highlights/sargostav2017.php"><h4>Sargostav 2017 PG intra<br>collegiate Cultural Fest  <br/><small>Sargotsav is the annual PG intra collegiate cultural fest organised by Kristu Jayanti College..
-                </small></h4></a>
-            </div>
-        </li>
-		
+        </li>-->
 		
 		
 		<li>
-            <img src="../../../../highlights/2017/australia.jpg" class="img-responsive"/>
-            <span class="date">16 Feb 2017</span>
-            <div class="quote">
-                <a href="../../../../highlights/ils_australia.php"><h4>Department of Social Work <br> International Lecture Series:<br> Social Work Licensing in<br> Australia<br/><small>On the 16th February 2017, as a part of the International Lecture Series, the Department of Social Work organised..
-                </small></h4></a>
-            </div>
-        </li>
-		
-		
-		
-		<li>
-            <img src="../../../../highlights/2017/child_protection.jpg" class="img-responsive"/>
+            <img src="../../../../highlights/2017/ieee.jpg" class="img-responsive"/>
             <span class="date">16 Feb 2017</span>
             <div class="quote">
                 <a href="../../../../highlights/ieee_workshop.php"><h4>IEEE One Day Workshop  <br/><small>The objective of this session was to be familiar with digital convergence for smart life. The sessions focused on topics relevant to both computer science and life science applications..
@@ -89,8 +39,9 @@
             </div>
         </li>
 		
+		
 		<li>
-            <img src="../../../../highlights/2017/child_protection.jpg" class="img-responsive"/>
+            <img src="../../../../highlights/2017/32_vichaarmantahn.jpg" class="img-responsive"/>
             <span class="date">21 Jan 2017</span>
             <div class="quote">
                 <a href="../../../../highlights/32edition_vichaarmanthan.php"><h4> 32<sup>nd</sup> edition of<br> Vichaarmanthan<br/><small>The event was titled on the theme 'Fundamental Duties'. The Honorable Justice emphasized the importance of the duties and responsibilities associated with being a citizen of India. ..
@@ -99,14 +50,31 @@
         </li>
 		
 		
+		<li>
+            <img src="../../../../highlights/2017/research_collqium.jpg" class="img-responsive"/>
+            <span class="date">26 Nov 2016</span>
+            <div class="quote">
+                <a href="../../../../highlights/research_colloquium.php"><h4>Research Colloquium Writing of<br> Research Proposal and<br> Funding Agencies<br/><small>Research colloquium is a programme organized by the department of Computer which is conducted once in every semester since 2011..
+                </small></h4></a>
+            </div>
+        </li>
 		
 		
+		
+		<!--<li>
+            <img src="../../../../highlights/2017/youthweek.jpg" class="img-responsive"/>
+            <span class="date">28-29 Sep 2016</span>
+            <div class="quote">
+                <a href="../../../../highlights/overall_trophy.php"><h4>Overall Winners @ National Level Intercollegiate <br/><small>Congratulations!! Our MCA Students for winning 'Overall Trophy' for the consecutive time at “Joshiana 7.0”, a National Level Intercollegiate IT Fest conducted by St. Joseph Engineering College, Mangalore, on 28th and 29th September 2016..
+                </small></h4></a>
+            </div>
+        </li>-->
 		
 		<li>
-            <img src="../../../../highlights/2017/youthweek.jpg" class="img-responsive"/>
-            <span class="date">12-21 Jan 2017</span>
+            <img src="../../../../highlights/2017/workshop_r.jpg" class="img-responsive"/>
+            <span class="date">26-27 Aug 2016</span>
             <div class="quote">
-                <a href="../../../../highlights/youthweek.php"><h4> Centre for Social Activities National Youth Week <br/><small>Centre for Social Activities has organized “ National Youth Week” from 12th to 21st January 2017 to commemorate the principles..
+                <a href="../../../../highlights/data_analysis_using_r.php"><h4>National Level Workshop on Data Analysis Using R  <br/><small>The Department of Computer Science (PG) conducted a Two day National Level workshop on "Data Analysis Using R" on 26th and 27th August 2016..
                 </small></h4></a>
             </div>
         </li>
