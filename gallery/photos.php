2d3b2e73f46a68566444686b400e89d1bffa7a38
JIJO JOSEPH <jijoputhiyath@gmail.com>
2017-06-16 15:58:45 +0530

    recent changes

--- gallery/photos.php
+++ gallery/photos.php
@@ -1,3 +1,4 @@
+<?php session_start();?>
 <!DOCTYPE html> <?php  $ROOT="http://".$_SERVER['HTTP_HOST']."/Github/kjc-website";/*$ROOT is the base URL & should on top of every PAGE */?> 
 <html lang="en" class="no-js">
     <head>
