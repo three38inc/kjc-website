c7b195d353d712972c62767095d0b94ec7c98a85
JIJO JOSEPH <jijoputhiyath@gmail.com>
2017-06-16 13:45:30 +0530

    changes

--- academics/College-Arts-Science-Commerce/Faculty-Sciences/department-cs-pg/index.php
+++ academics/College-Arts-Science-Commerce/Faculty-Sciences/department-cs-pg/index.php
@@ -1,4 +1,5 @@
-<!DOCTYPE html> <?php  $ROOT="http://".$_SERVER['HTTP_HOST']."/Github/kjc-website";/*$ROOT is the base URL & should on top of every PAGE */?> 
+<!DOCTYPE html>
+<?php  $ROOT="http://".$_SERVER['HTTP_HOST']."/Github/kjc-website";$imgNum=mt_rand(1,4);/*$ROOT is the base URL & should on top of every PAGE */?>
 <html lang="en" class="no-js">
 
 <head>
@@ -9,11 +10,12 @@
     <!--<meta name="description" content="Sidebar Transitions: Transition effects for off-canvas views" />
 <meta name="keywords" content="transition, off-canvas, navigation, effect, 3d, css3, smooth" />-->
     <meta name="author" content="Three38 inc" />
-    <link rel="shortcut icon" href="../../../images/icon.png">
+
+    <link rel="shortcut icon" href="../../../../images/icon.png">
     <!-- Bootstrap css -->
     <link href="../../../../bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <!--additional css-->
-    <link rel="stylesheet" type="text/css" href="../../css/component.css" />
+    <link rel="stylesheet" type="text/css" href="../../../../css/component.css" />
     <link rel="stylesheet" href="../../../../css/font-awesome.css">
     <link rel="stylesheet" href="../../../../css/font-awesome.min.css">
     <link rel="stylesheet" href="../../../../css/roundIcons.css">
@@ -34,6 +36,7 @@
     <script src="../../../../js/respond.js"></script>
     <script src="../../../../js/custom.js"></script>
     <script src="../../../../js/fakeLoader.js"></script>
+
 
 </head>
 
@@ -64,68 +67,61 @@
             <?php include ('php/navbar.php');  ?>
         </div>
     </div>
-    
+    <div id="st-container" class="st-container">
+        <?php include('../../../../php/offCanvasMenu.php'); ?>
         <!-- content push wrapper -->
         <div class="st-pusher">
             <div class="st-content">
                 <!-- this is the wrapper for the content -->
                 <div class="st-content-inner">
-
-                    <div class="row" style="height: 110px;"></div>
-                    <div class="row">
-                        <div class="col-lg-12" style="position: relative;">
-                            <div id="lawCarousel" class="carousel slide" data-ride="carousel" style="margin-top: 0;">
-                                <div class="carousel-inner" role="listbox">
-                                    <div class="item active">
-                                        <img src="images/img1.jpg" class="img-responsive" />
-                                    </div>
-                                    <div class="item">
-                                        <img src="images/img2.jpg" />
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-8 col-md-8 hidden-xs hidden-sm floatDiv">
-                                <div class="col-lg-2"></div>
-                               
-                            </div>
-
-                            <div class="col-xs-12 col-sm-12 hidden-md hidden-lg">
-                                <?php include "php/deanary.php"; ?>
-                            </div>
-                        </div>
-                    </div>
-
                     <div class="container">
+                        <div class="row empty" style="margin-bottom:9vh;"></div>
+                        <div class="row" style="margin-top: 100px; padding-left: 5px; padding-right: 5px;">
+<!--
+                            <img src="images/CAROUSEL/<?php echo $imgNum?>.jpg" class="img-responsive" style="margin-bottom:4px;height: 330px;
+    width: 100%;" />
+-->
+                            <?php include ('php/carousel.php'); ?>
+                        </div>
+
+
+
                         <div class="row">
                             <div class="col-lg-9">
-                                <div class="content-spl myWelcome wow fadeIn" style="margin-bottom:1px;">
-                                    <?php include ('php/welcome.php'); ?>
+                                <div class="content-left myWelcome wow fadeIn" style="margin-left: 0px;margin-right: 0px;">
+
+
+                                    <?php include('php/welcome.php');?>
+
                                 </div>
-                                <div class="content-spl myNav wow fadeIn" style="height: auto;min-height: 284px;padding-top: 0;">
+                                <div class="content-spl myNav wow fadeIn" style="height: auto;min-height: 284px;padding-top: 0;margin-top: 8px;">
                                     <?php include ('php/accordion-pill-tabs.php'); ?>
                                     <div style="clear: both;"></div>
                                 </div>
-                                <div style="clear: both;"></div>
-                            </div>
-                            <div class="col-lg-3">
-                                <div class="content-spl wow fadeIn" style="height: 550px; overflow-y:hidden;">
-                                    <?php include ('php/newsAndUpdates.php'); ?>
-                                </div>
-                            </div>
-                            <div class="clearfill"></div>
-                        </div>
 
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="content-spl wow fadeIn">
+                                <div class="content-spl wow fadeIn" style="margin-top: 9px;">
                                     <?php include('php/highLights.php');?>
                                 </div>
+                                <div style="clear: both;"></div>
+                            </div>
+                            <div class="col-lg-3" style="margin-top:1em;">
+                                <div class="content-spl wow fadeIn" style="height: 400px; overflow-y:hidden;margin-top:-6px;">
+                                    <?php include ('php/dept_acheivements.php'); ?>
+                                </div>
+                                <div class="content-spl wow fadeIn" style="height: 508px; overflow-y:hidden;margin-top: 15px;">
+                                    <?php include ('php/newsAndUpdates.php'); ?>
+                                </div>
+                                <div class="clearfill"></div>
+
+
                             </div>
                         </div>
+
                     </div>
 
                     <?php include ('../../../../php/alerts.php');  ?>
                     <?php include('../../../../php/footer.php'); ?>
+
                 </div>
 
 
@@ -133,11 +129,13 @@
             </div>
             <!--/st-content-inner-->
         </div>
-        <!--st-pusher-->
-    
-    <!--st-container-->
+        <!-- this is the wrapper for the content-->
+    </div>
+    <!--st-pusher-->
+    <!--        </div> st-container -->
 
-    <script src="../../../../js/cbpFWTabs.js"></script>
+    <!--<script src="../js/cbpFWTabs.js"></script>-->
+
     <script>
         (function() {
 
@@ -154,29 +152,263 @@
     <script src="../../../../js/swipeview.js"></script>
     <script src="../../../../js/jquery.bootstrap.newsbox.min.js"></script>
     <script src="../../../../js/classie.js"></script>
-    <!--	<script src="../../js/morphingSearch.js"></script>-->
+    <!--    <script src="../../../../js/morphingSearch.js"></script>-->
     <script src="../../../../js/sidebarEffects.js"></script>
     <script src="../../../../js/transit.js"></script>
     <script src="../../../../js/sly.js"></script>
     <script src="../../../../js/customSly.js"></script>
     <script src="../../../../js/wow.min.js"></script>
-    
-     <script src="../../../../js/owl.carousel.min.js" type="text/javascript"></script>
+    <script src="../../../../js/owl.carousel.min.js" type="text/javascript"></script>
     <script src="../../../../js/scripts.js"></script>
-    <script src="js/manSchool.js"></script>
-
     <script type="text/javascript">
         function googleTranslateElementInit() {
             new google.translate.TranslateElement({
                 pageLanguage: 'en'
             }, 'google_translate_element');
         }
-        
 
     </script>
     <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+    
     <script>
-        $(document).ready(function(){
+        $(document).ready(function() {
+
+            var $scene = $(".scene"),
+                $content = $(".content"),
+                $back = $(".back"),
+                $backBgs = $(".back__bg"),
+                $front = $(".front"),
+                $frontBgs = $(".front__bg"),
+                $menuBlock = $(".menu__block"),
+                $svgPath = $(".menu__block-svgPath"),
+                animating = false,
+                menuActive = false,
+                menuAnimTime = 600,
+                blockAnimTime = 1500,
+                $sliderCont = $(".menu-slider__content"),
+                curSlide = 1,
+                sliderXDiff = 0,
+                curPage = 1,
+                nextPage = 0,
+                numOfPages = $(".front__bg").length,
+                scaleTime = 500,
+                transTime = 500,
+                totalTime = scaleTime + transTime,
+                changeTimeout,
+                timeoutTime = 8000,
+                winW = $(window).width(),
+                winH = $(window).height();
+
+            // init nav element timeout animation
+            $(".nav__el-1").addClass("active");
+
+            //default debounce function from David Walsh blog
+            function debounce(func, wait, immediate) {
+                var timeout;
+                return function() {
+                    var context = this,
+                        args = arguments;
+                    var later = function() {
+                        timeout = null;
+                        if (!immediate) func.apply(context, args);
+                    };
+                    var callNow = immediate && !timeout;
+                    clearTimeout(timeout);
+                    timeout = setTimeout(later, wait);
+                    if (callNow) func.apply(context, args);
+                };
+            };
+
+            function changePages() {
+                $(".back__bg, .front__bg, .nav__el").removeClass("active");
+                $(".nav__el-" + curPage).addClass("active");
+                $back.css("transform", "translate3d(0," + (curPage - 1) * -100 + "%,0)");
+                $front.css("transform", "translate3d(0," + (curPage - 1) * 100 + "%,0)");
+                createTimeout();
+
+                setTimeout(function() {
+                    $(".back__bg-" + curPage + ", .front__bg-" + curPage).addClass("active");
+                }, totalTime);
+            };
+
+            $(document).on("click", ".nav__el:not(.active)", function() {
+                curPage = $(this).attr("data-page");
+                changePages();
+            });
+
+            // ugly hack for animation reset when you coming back from menu section
+            function resetTimeoutAnimation() {
+                var $activeNavEl = $(".nav__el.active"),
+                    $activeParts = $activeNavEl.find(".nav__el-clone, .nav__el")
+                $activeParts.addClass("instant");
+                $activeNavEl.removeClass("active");
+                $activeParts.css("top");
+                $activeParts.removeClass("instant");
+                $activeParts.css("top");
+                $activeNavEl.addClass("active");
+            }
+
+            /* creates timeOut for change of slides.
+            Call's itself from inside of changePages() function
+            */
+            function createTimeout() {
+                window.clearTimeout(changeTimeout);
+                resetTimeoutAnimation();
+                changeTimeout = setTimeout(function() {
+                    if (curPage >= numOfPages) {
+                        curPage = 1;
+                    } else {
+                        curPage++;
+                    }
+                    changePages();
+                }, timeoutTime);
+            };
+
+            createTimeout();
+
+            /* creates path D attribute strings for animation
+            initial d = fullScreen
+            mid d = Q curves with 5% padding
+            final d = centered 90% width/height block
+            */
+            function createD(type) {
+                var types = {
+                    "init": ["M0,0",
+                        "Q" + winW / 2 + ",0",
+                        winW + ",0",
+                        "Q" + winW + "," + winH / 2,
+                        winW + "," + winH,
+                        "Q" + winW / 2 + "," + winH,
+                        "0," + winH,
+                        "Q0," + winH / 2,
+                        "0,0"
+                    ],
+                    "mid": ["M0,0",
+                        "Q" + winW / 2 + "," + winH * 0.05,
+                        winW + ",0",
+                        "Q" + winW * 0.95 + "," + winH / 2,
+                        winW + "," + winH,
+                        "Q" + winW / 2 + "," + winH * 0.95,
+                        "0," + winH,
+                        "Q" + winW * 0.05 + "," + winH / 2,
+                        "0,0"
+                    ],
+                    "final": ["M" + winW * 0.05 + "," + winH * 0.05,
+                        "Q" + winW / 2 + "," + winH * 0.05,
+                        winW * 0.95 + "," + winH * 0.05,
+                        "Q" + winW * 0.95 + "," + winH / 2,
+                        winW * 0.95 + "," + winH * 0.95,
+                        "Q" + winW / 2 + "," + winH * 0.95,
+                        winW * 0.05 + "," + winH * 0.95,
+                        "Q" + winW * 0.05 + "," + winH / 2,
+                        winW * 0.05 + "," + winH * 0.05
+                    ]
+                };
+                return types[type].join(" ");
+            }
+
+            // animates path d with SnapSVG
+            function animateBlock(reverse) {
+                winW = $(window).width();
+                winH = $(window).height();
+                var initD = createD("init"),
+                    midD = createD("mid"),
+                    finalD = createD("final");
+
+                if (!reverse) {
+                    $svgPath.attr("d", initD);
+                    Snap($svgPath[0]).animate({
+                        "path": midD
+                    }, blockAnimTime / 2, mina.elastic, function() {
+                        Snap($svgPath[0]).animate({
+                            "path": finalD
+                        }, blockAnimTime / 2, mina.elastic);
+                    });
+                } else {
+                    Snap($svgPath[0]).animate({
+                        "path": midD
+                    }, blockAnimTime / 2, mina.elastic, function() {
+                        Snap($svgPath[0]).animate({
+                            "path": initD
+                        }, blockAnimTime / 2, mina.elastic);
+                    });
+                }
+
+            };
+
+            // resizes opened menu path d block, because i can't change viewBox with js
+            var resizeSvg = debounce(function() {
+                winW = $(window).width();
+                winH = $(window).height();
+                $svgPath.attr("d", createD("final"));
+            }, 50);
+
+            // default madness
+            $(document).on("click", ".menu__btn", function() {
+                if (animating) return;
+                animating = true;
+                setTimeout(function() {
+                    animating = false;
+                }, blockAnimTime + menuAnimTime);
+
+                if (!menuActive) {
+                    menuActive = true;
+                    window.clearTimeout(changeTimeout);
+                    $content.addClass("inactive");
+                    $scene.addClass("menu-visible");
+                    $(".back__bg:not(.active)").addClass("hidden");
+                    $(window).on("resize", resizeSvg);
+                    setTimeout(function() {
+                        $menuBlock.addClass("visible");
+                        animateBlock();
+                    }, menuAnimTime);
+                } else {
+                    menuActive = false;
+                    animateBlock(true);
+                    setTimeout(function() {
+                        $menuBlock.removeClass("visible");
+                        createTimeout();
+                        $(".back__bg").removeClass("hidden");
+                        $content.removeClass("inactive");
+                        $scene.removeClass("menu-visible");
+                    }, blockAnimTime);
+                    $(window).off("resize");
+                }
+            });
+
+        });
+
+    </script>
+    <script>
+        $("document").ready(function() {
+            //below code is for retrieving corresponding pages #START
+            var activePage = 'page1';
+            $.ajax({
+                url: 'snipets/' + activePage + '.php',
+                success: function(data) {
+                    $('#page').html(data);
+                },
+                error: function(data) {
+                    alert('failed to load data');
+                }
+            });
+            $('.profile-pagination li a').click(function() {
+                $(this).parent().siblings().removeClass('active');
+                $(this).parent().addClass('active');
+            });
+            $('.profile-pagination li a').click(function() {
+                var activePage = this.getAttribute('data-value');
+                $.ajax({
+                    url: 'snipets/' + activePage + '.php',
+                    success: function(data) {
+                        $('#page').html(data);
+                    },
+                    error: function(data) {
+                        alert('failed to load data');
+                    }
+                });
+            });
+            //above code is for retrieving corresponding pages #END
             $('#frameHighLights').owlCarousel({
                 items: 1,
                 loop: true,
@@ -204,7 +436,16 @@
                     }
                 }
             });
+            $('#MediaSlider').owlCarousel({
+                loop: true,
+                autoplay: true,
+                margin: 10,
+                nav: false,
+                dots: true,
+                items: 1
+            });
         });
+
     </script>
 </body>
 
