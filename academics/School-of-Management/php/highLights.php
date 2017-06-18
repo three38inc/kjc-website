db3adff11d432f91e15c53924200f4125488d1de
JIJO JOSEPH <jijoputhiyath@gmail.com>
2017-06-19 01:06:19 +0530

    changes

--- academics/School-of-Management/php/highLights.php
+++ academics/School-of-Management/php/highLights.php
@@ -8,110 +8,167 @@
 <div class="frame-testimonials" style="margin-top: 10px;">
     <ul id="frameHighLights" class="owl-carousel">
         <li>
-            <img src="images/highlights/vinimay_leadership_series.jpg" class="img-responsive"/>
+            <img src="images/highlights/vinimay_leadership_series.jpg" class="img-responsive" />
             <span class="date">21<sup>st</sup> July 2015</span>
             <div class="quote">
-                <a href="../../events/events.php?mode=openlink&year=2015&month=july&url=vinimay.php"><h4>Vinimay, a leadership series programme<br/><small>
-				The department of Management Studies (MBA and PGDM) and department of Computer Science (PG) organized..</small></h4></a>
+                <a href="../../events/events.php?mode=openlink&year=2015&month=july&url=vinimay.php">
+                    <h4>Vinimay, a leadership series programme<br/><small>
+				The department of Management Studies (MBA and PGDM) and department of Computer Science (PG) organized..</small></h4>
+                </a>
             </div>
         </li>
         <li>
-            <img src="images/highlights/panel_discussion_unionbudget.jpg" class="img-responsive"/>
+            <img src="images/highlights/panel_discussion_unionbudget.jpg" class="img-responsive" />
             <span class="date">1<sup>st</sup> August 2016</span>
             <div class="quote">
-              <a href="../../events/events.php?mode=openlink&year=2016&month=august&url=panel_discussion_union_budget.php"><h4>Panel discussion on UNION BUDGET 2016-2017  <br/><small>The program started at 2.00 pm in SKE auditorium. Dr. B.L Bhagyalakshmi, Former Director..</small></h4></a>
+                <a href="../../events/events.php?mode=openlink&year=2016&month=august&url=panel_discussion_union_budget.php">
+                    <h4>Panel discussion on UNION BUDGET 2016-2017 <br/><small>The program started at 2.00 pm in SKE auditorium. Dr. B.L Bhagyalakshmi, Former Director..</small></h4>
+                </a>
             </div>
         </li>
         <li>
-            <img src="images/highlights/3i_16january.jpg" class="img-responsive"/>
+            <img src="images/highlights/3i_16january.jpg" class="img-responsive" />
             <span class="date">16<sup>th</sup> Jan 2016</span>
             <div class="quote">
-              <a href="../../events/events.php?mode=openlink&year=2016&month=january&url=industry_institute_interface_series_jan16.php"><h4>Industry Institute Interface Series  <br/><small>3i Program is one of the series conducted in Kristu Jayanti School of Management ..</small></h4></a>
+                <a href="../../events/events.php?mode=openlink&year=2016&month=january&url=industry_institute_interface_series_jan16.php">
+                    <h4>Industry Institute Interface Series <br/><small>3i Program is one of the series conducted in Kristu Jayanti School of Management ..</small></h4>
+                </a>
             </div>
         </li>
         <li>
-            <img src="images/highlights/3i_5dec.jpg" class="img-responsive"/>
+            <img src="images/highlights/3i_5dec.jpg" class="img-responsive" />
             <span class="date">5<sup>th</sup> december 2015</span>
             <div class="quote">
-              <a href="../../events/events.php?mode=openlink&year=2015&month=december&url=3i_program_5dec.php"><h4>Industry Institute Interface Series  <br/><small>3i Program is one of the series conducted in Kristu Jayanti School of Management ..</small></h4></a>
+                <a href="../../events/events.php?mode=openlink&year=2015&month=december&url=3i_program_5dec.php">
+                    <h4>Industry Institute Interface Series <br/><small>3i Program is one of the series conducted in Kristu Jayanti School of Management ..</small></h4>
+                </a>
             </div>
         </li>
         <li>
-            <img src="images/highlights/knowledge_exchange_programme.jpg" class="img-responsive"/>
+            <img src="images/highlights/knowledge_exchange_programme.jpg" class="img-responsive" />
             <span class="date">30<sup>th</sup> september 2015</span>
             <div class="quote">
-              <a href="../../events/events.php?mode=openlink&year=2015&month=september&url=knowledge_exchange_programme.php"><h4>Knowledge Exchange Programme Of Hnu University (Germany)<br/><small>The knowledge exchange programme was conducted on the 30th of September..</small></h4></a>
+                <a href="../../events/events.php?mode=openlink&year=2015&month=september&url=knowledge_exchange_programme.php">
+                    <h4>Knowledge Exchange Programme Of Hnu University (Germany)<br/><small>The knowledge exchange programme was conducted on the 30th of September..</small></h4>
+                </a>
             </div>
         </li>
         <li>
-            <img src="images/highlights/hadoop.jpg" class="img-responsive"/>
+            <img src="images/highlights/hadoop.jpg" class="img-responsive" />
             <span class="date">28<sup>th</sup> march 2015</span>
             <div class="quote">
-              <a href="../../events/events.php?mode=openlink&year=2015&month=march&url=simplifying_big_data.php"><h4>MDP on Simplifying big data with Hadoop<br/><small>The Management development programme on big data was held on the 28th of March 2015..</small></h4></a>
+                <a href="../../events/events.php?mode=openlink&year=2015&month=march&url=simplifying_big_data.php">
+                    <h4>MDP on Simplifying big data with Hadoop<br/><small>The Management development programme on big data was held on the 28th of March 2015..</small></h4>
+                </a>
             </div>
         </li>
         <li>
-            <img src="images/highlights/ngo_visit.jpg" class="img-responsive"/>
+            <img src="images/highlights/ngo_visit.jpg" class="img-responsive" />
             <span class="date">17<sup>th</sup> april 2015</span>
             <div class="quote">
-              <a href="../../events/events.php?mode=openlink&year=2015&month=april&url=ngo_visit.php"><h4>The NGO visit<br/><small>The NGO visit for the MBA and PGDM batch was on 17th April 2015. The students were divided into two teams..</small></h4></a>
+                <a href="../../events/events.php?mode=openlink&year=2015&month=april&url=ngo_visit.php">
+                    <h4>The NGO visit<br/><small>The NGO visit for the MBA and PGDM batch was on 17th April 2015. The students were divided into two teams..</small></h4>
+                </a>
             </div>
         </li>
         <li>
-            <img src="images/highlights/vinimay_leadership.jpg" class="img-responsive"/>
+            <img src="images/highlights/vinimay_leadership.jpg" class="img-responsive" />
             <span class="date">19<sup>th</sup> april 2015</span>
             <div class="quote">
-              <a href="../../events/events.php?mode=openlink&year=2015&month=april&url=vinimay_leadership.php"><h4>The vinimay leadership series<br/><small>The vinimay leadership series was held on the 19th April 2015 jointly for the MBA and MCA batch..</small></h4></a>
+                <a href="../../events/events.php?mode=openlink&year=2015&month=april&url=vinimay_leadership.php">
+                    <h4>The vinimay leadership series<br/><small>The vinimay leadership series was held on the 19th April 2015 jointly for the MBA and MCA batch..</small></h4>
+                </a>
             </div>
         </li>
         <li>
-            <img src="images/highlights/icgclb_2015.jpg" class="img-responsive"/>
+            <img src="images/highlights/icgclb_2015.jpg" class="img-responsive" />
             <span class="date">30<sup>th</sup> june 2015</span>
             <div class="quote">
-              <a href="../../events/events.php?mode=openlink&year=2015&month=june&url=international_conference_globalisation_changing_landscape_business.php"><h4>International Conference<br/><small>Krisym, Kristu Jayanti International symposium ..</small></h4></a>
+                <a href="../../events/events.php?mode=openlink&year=2015&month=june&url=international_conference_globalisation_changing_landscape_business.php">
+                    <h4>International Conference<br/><small>Krisym, Kristu Jayanti International symposium ..</small></h4>
+                </a>
             </div>
         </li>
         <li>
-            <img src="images/highlights/annual_rep.jpg" class="img-responsive"/>
+            <img src="images/highlights/annual_rep.jpg" class="img-responsive" />
             <span class="date">13<sup>th</sup> august 2015</span>
             <div class="quote">
-              <a href="../../events/events.php?mode=openlink&year=2015&month=august&url=gramasamveditha_2015.php"><h4>GRAMASAMVEDITHA - RURAL EXPOSURE PROGRAMME [REP]<br/><small>Kristu Jayanti School of Management organized its annual REP for First Trimester ..</small></h4></a>
+                <a href="../../events/events.php?mode=openlink&year=2015&month=august&url=gramasamveditha_2015.php">
+                    <h4>GRAMASAMVEDITHA - RURAL EXPOSURE PROGRAMME [REP]<br/><small>Kristu Jayanti School of Management organized its annual REP for First Trimester ..</small></h4>
+                </a>
             </div>
         </li>
         <li>
-            <img src="images/highlights/millionaire_map.jpg" class="img-responsive"/>
+            <img src="images/highlights/millionaire_map.jpg" class="img-responsive" />
             <span class="date">7<sup>th</sup> august 2015</span>
             <div class="quote">
-              <a href="../../events/events.php?mode=openlink&year=2015&month=august&url=millionaire_map.php"><h4>Millionaire Map" International Lecture Series (ILS)<br/><small>The Millionaire's Map is the first book in history to integrate this mysterious code..</small></h4></a>
+                <a href="../../events/events.php?mode=openlink&year=2015&month=august&url=millionaire_map.php">
+                    <h4>Millionaire Map" International Lecture Series (ILS)<br/><small>The Millionaire's Map is the first book in history to integrate this mysterious code..</small></h4>
+                </a>
             </div>
         </li>
         <li>
-            <img src="images/highlights/3i_19_sep.jpg" class="img-responsive"/>
+            <img src="images/highlights/3i_19_sep.jpg" class="img-responsive" />
             <span class="date">19<sup>th</sup> september 2015</span>
             <div class="quote">
-              <a href="../../events/events.php?mode=openlink&year=2015&month=september&url=3i_19_sep.php"><h4>Industry Institute Interface Series<br/><small>3i program is one of the series conducted in Kristu Jayanti School of Management where various Managers..</small></h4></a>
+                <a href="../../events/events.php?mode=openlink&year=2015&month=september&url=3i_19_sep.php">
+                    <h4>Industry Institute Interface Series<br/><small>3i program is one of the series conducted in Kristu Jayanti School of Management where various Managers..</small></h4>
+                </a>
             </div>
         </li>
         <li>
-            <img src="images/highlights/vinimay2015.jpg" class="img-responsive"/>
-            <span class="date">09<sup>th</sup> september 2015</span>
+            <img src="images/highlights/rbi_executive.jpg" class="img-responsive" />
+            <span class="date">11<sup>th</sup> september 2015</span>
             <div class="quote">
-              <a href="../../events/events.php?mode=openlink&year=2015&month=september&url=3i_19_sep.php"><h4>Industry Institute Interface Series<br/><small>3i program is one of the series conducted in Kristu Jayanti School of Management where various Managers..</small></h4></a>
+                <a href="../../events/events.php?mode=openlink&year=2015&month=september&url=deepali_pant_joshi.php">
+                    <h4>Vichaarmanthan 2015 - RBI Reforms and Current Trends in Banking<br/><small>Ms. Deepali Pant Joshi, Executive Director, Reserve Bank of India; was the key note speaker for the twenty fourth edition of Vicharmanthan</small></h4>
+                </a>
             </div>
         </li>
-        
-       
-    
-		
-		
-		
-	
-	
-		
-	
-		
-		
-		
-		
+        <li>
+            <img src="images/highlights/india_reinventing.jpg" class="img-responsive" />
+            <span class="date">1<sup>st</sup> august 2015</span>
+            <div class="quote">
+                <a href="../../events/events.php?mode=openlink&year=2015&month=august&url=india_reinventing.php">
+                    <h4>Panel Discussion [Make in India-Reinventing Skills]<br/><small>The department of management studies of Kristu Jayanti School of Management had organized a Panel Discussion .. </small></h4>
+                </a>
+            </div>
+        </li>
+        <li>
+            <img src="images/highlights/talento2015.jpg" class="img-responsive" />
+            <span class="date">10<sup>th</sup> september 2015</span>
+            <div class="quote">
+                <a href="../../events/events.php?mode=openlink&year=2015&month=september&url=pg_management_fest_talento_2015.php">
+                    <h4>PG Management fest TALENTO 2015 launched<br/><small>The Department of Management Studies (MBA &amp; PGDM) organized a two day .. </small></h4>
+                </a>
+            </div>
+        </li>
+        <li>
+            <img src="images/highlights/singapore_hongkong.jpg" class="img-responsive" />
+            <span class="date">30<sup>th</sup> september 2015</span>
+            <div class="quote">
+                <a href="../../events/events.php?mode=openlink&year=2015&month=september&url=international_visit_singapore_hongkong.php">
+                    <h4>International visit to Singapore and Hong Kong<br/><small>The Department of Management Studies, conducted an International Industry visit and Exposure to Singapore and Hong Kong .. </small></h4>
+                </a>
+            </div>
+        </li>
+        <li>
+            <img src="images/highlights/niranjan_khatri.jpg" class="img-responsive" />
+            <span class="date">12<sup>th</sup> september 2015</span>
+            <div class="quote">
+                <a href="../../events/events.php?mode=openlink&year=2015&month=august&url=expert_lecture_series_niranjan.php">
+                    <h4>EXPERT LECTURE SERIES<br/><small>The department of Management Studies (MBA and PGDM) organized 'Expert Lecture Series' on August 12, 2015.. .. </small></h4>
+                </a>
+            </div>
+        </li>
+        <li>
+            <img src="images/highlights/meaning_to_life.jpg" class="img-responsive" />
+            <span class="date">25<sup>th</sup> july 2015</span>
+            <div class="quote">
+                <a href="../../events/events.php?mode=openlink&year=2015&month=july&url=meaning_to_life.php">
+                    <h4>3i Industry Institute Interface) Series "Meaning to Life"<br/><small>Industry Institute Interface popularly termed as 3i is an initiative to bridge the gap.. </small></h4>
+                </a>
+            </div>
+        </li>
     </ul>
 </div>
