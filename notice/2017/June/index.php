21c9597dfb000da6610639209ebddf490db5994d
PraveenKumarkjc <pravisweets@gmail.com>
2017-06-22 15:15:17 +0530

    notice

--- notice/2017/June/index.php
+++ notice/2017/June/index.php
@@ -2,6 +2,50 @@
     <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
 		
 		
+		   <div class="panel panel-default">
+            <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse2" aria-expanded="false" aria-controls="collapse3">
+                <div class="panel-heading" role="tab" id="heading3">
+                    <h4 class="panel-title">
+
+                      June 22<sup>nd</sup>
+                        <i class="fa fa-caret-right pull-right "></i>
+                    </h4>
+
+                </div>
+            </a>
+            <div id="collapse3" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading3">
+                <div class="panel-body">
+                   
+  <p><b>For UG I Semester classes only</b><br>Students (Boys/Girls) who are interested to join NCC for this academic year 2017-18 can collect the application form from the Reception Counter (Main Block) and submit the duly filled application form on or before 27th June 2017 at the reception counter. For further details contact Lt. Sarvesh, NCC Officer in Staff Room No. X – PG Block (Interview will be conducted on 28th & 29th of June 2017 at NCC Room, III Floor, UG Block.</p>
+
+		  <p><b>For all UG & PG classes (except MBA & PGDM)</b><br>
+			  <b>Kristu Jayanti College Gymnasium</b><br>
+
+    Kristu Jayanti College Gymnasium is set up in the 5th Floor of PG Block. Registrations are open. Interested students should register their names at Counter No. 2 (UG Office). Registration is compulsory for using the facilities in the gym.<br>
+
+    Registration Details<br>
+    i. Registration fee for UG & PG students:  Rs. 1500/- (Till the completion of current course)<br>
+    ii.Monthly fee: Rs. 300/- (has to be paid on or before 5th working day of the month)<br>
+
+    Timings: 06.00 a.m. – 08.30 a.m.<br>
+                  03.30 p.m. – 08.30 p.m.<br>
+
+    > Instructor will be available for guidance and training in the gym.<br>
+			  > Defaulters of the monthly fees will be not allowed to use the gym facilities.</p>
+
+
+		  <p><b>For II/III Year UG classes, II Year PG classes and II/III Year MCA classes</b><br>
+			  <b>Payment of Fees</b><br>
+
+			  Tomorrow (23/06/2017) onwards the mode of fee Payment will be  Demand Draft (DD) only in favour of The Principal, Kristu Jayanti College, payable at Bengaluru.</p>
+                </div>
+            </div>
+        </div>
+		
+		
+	<!-------------------------------------------------------------------end-------------------------------------------------------------------------------------->	
+
+		
 		
 				
         <div class="panel panel-default">
