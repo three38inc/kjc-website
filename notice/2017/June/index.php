30903667818ef6ec0306636129dd67601972d260
PraveenKumarkjc <pravisweets@gmail.com>
2017-06-22 11:34:01 +0530

    changes

---
+++ notice/2017/June/index.php
@@ -0,0 +1,107 @@
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 event-tab">
+    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+		
+		
+		
+				
+        <div class="panel panel-default">
+            <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse2" aria-expanded="false" aria-controls="collapse2">
+                <div class="panel-heading" role="tab" id="heading2">
+                    <h4 class="panel-title">
+
+                      June 21<sup>st</sup>
+                        <i class="fa fa-caret-right pull-right "></i>
+                    </h4>
+
+                </div>
+            </a>
+            <div id="collapse2" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading2">
+                <div class="panel-body">
+                   
+    <p><b>For all UG & PG classes</b><br>
+
+			  Let us join together to keep our Campus clean and green. Students are requested to take special care to keep our parking place neat and tidy always. Please do not throw anything on the floor. Deposit all waste paper/materials in the dust bins provided at different places in the campus. Please switch off the lights and fans in the class rooms when you leave the class rooms, and turn off water taps properly after use.</p>
+
+		  <p><b>International Yoga Day Celebration</b><br>
+
+    Our college is celebrating the Yoga Day 2017 in our campus today, 21st June 2017. Those who are interested to take part in this event have to bring the “yoga dress”.<br>
+
+    Venue I:  Quadrangle (UG Block)<br>
+    Time: 10.50 a.m.<br>
+
+    Venue II: Mini Auditorium (PG Block) for MCA classes only<br>
+    Time: 11.15 a.m.<br>
+
+			  Note: This event is compulsory for the NSS volunteers.</p>
+
+ 
+		  <p><b>For II/III Year UG classes, II Year PG classes and II/III Year MCA classes</b><br>
+
+    Tomorrow (22/06/2017) is the last date to pay the tuition fee for the academic year 2017-18.  <br>
+
+    Venue: Mini Auditorium I (II Floor) (The South Indian Bank Counter)<br>
+			  Time:  10.30 a.m. - 4.30 p.m.</p>
+
+
+		  <p><b>For I Year UG classes</b><br>
+
+			  Suggestion box is available near the Reception Counter (Main Block). Suggestions are welcome for further improvements in academic excellence and progress.</p>
+
+                </div>
+            </div>
+        </div>
+		
+		
+	<!-------------------------------------------------------------------end-------------------------------------------------------------------------------------->	
+		
+		
+        <div class="panel panel-default">
+            <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse1" aria-expanded="false" aria-controls="collapse1">
+                <div class="panel-heading" role="tab" id="heading1">
+                    <h4 class="panel-title">
+
+                       June 20<sup>th</sup>
+                        <i class="fa fa-caret-right pull-right "></i>
+                    </h4>
+                </div>
+            </a>
+            <div id="collapse1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading1">
+                <div class="panel-body">
+                    
+  <p><b>For UG I Semester classes only</b><br>
+			  Students (Boys/Girls) who are interested to join NCC for this academic year 2017-18 can collect the application form from the Reception Counter (Main Block) and submit the duly filled application form on or before 27th June 2017 at the reception. For further details contact Lt. Sarvesh, NCC Officer in Staff Room No. X – PG Block (Interview will be conducted on 28th & 29th of June 2017 at NCC Room, III Floor, UG Block).</p>
+
+		  <p><b>For all UG & PG classes</b><br>
+			  From 21/06/2017 onwards there will be selection trials for students who wish to join the college team (Basketball, Football, Volleyball, Cricket, Throwball, Badminton, Table Tennis and Athletics). Details are displayed on the college notice board.</p>
+
+		  <p><b>International Yoga Day Celebration</b><br>
+			  Our college is going to celebrate the Yoga Day 2017 in our campus on 21st June 2017. Those who are interested to take part in this event have to bring the “yoga dress”.<br>
+
+			  <b>Venue I:</b>  Quadrangle (UG Block)<br>
+			  <b>Time:</b> 10.50 a.m.<br>
+
+			  <b>Venue II:</b> Quadrangle (PG Block) for MCA classes only<br>
+			  <b>Time:</b> 11.15 a.m.<br>
+
+			  <b>Note:</b> This event is compulsory for the NSS volunteers.</p>
+
+
+		  <p><b>Kristu Jayanti College Gymnasium</b><br>
+ Kristu Jayanti College Gymnasium is set up in the 5th Floor of PG Block. Registrations are open. Interested students should register their names at Counter No. 2 (UG Office). Registration is compulsory for using the facilities in the gym.<br>
+
+    Registration Details<br>
+    i. Registration fee for UG & PG students:  Rs. 1500/- (Till the completion of current course)<br>
+
+    ii. Monthly fee: Rs. 300/- (has to be paid on or before 5th working day of the month)<br>
+
+			  <b>Timings:</b>    06.00 a.m. – 08.30 a.m.<br>
+                      03.30 p.m. – 08.30 p.m.<br>
+
+    Instructor will be available for guidance and training in the gym.<br>
+			  Defaulters of the monthly fees will be not allowed to use the gym facilities.</p>
+ 
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
