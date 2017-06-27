1000fdcfafc35710c08c4383574d6aa2880a3119
JIJO JOSEPH <jijoputhiyath@gmail.com>
2017-06-27 13:49:13 +0530

    changes

---
+++ apps/alerts/snippets/connect.php
@@ -0,0 +1,14 @@
+<?php
+    $server = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $mydb = 'kjc_website_alert';
+    $link  = mysqli_connect($server, $user, $pass,$mydb);
+    if (!$link) {
+        echo "Error: Unable to connect to MySQL." . PHP_EOL;
+        echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+        exit;
+    }
+
+?>
