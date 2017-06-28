8e86f7d5b81f6aef468abdfb0e8567d7c03c8b80
PraveenKumarkjc <pravisweets@gmail.com>
2017-06-28 15:09:44 +0530

    changes

--- academics/College-Arts-Science-Commerce/Faculty-Sciences/department-cs-pg/php/highLights.php
+++ academics/College-Arts-Science-Commerce/Faculty-Sciences/department-cs-pg/php/highLights.php
@@ -14,7 +14,7 @@
             <img src="../../../../highlights/2017/enthios_2K17.jpg" class="img-responsive"/>
             <span class="date">21-22 Feb 2017</span>
             <div class="quote">
-                <a href="../../../../events/events.php?mode=openlink&year=2017&month=february&url=enthios_2K17.php"><h4> "Enthios 2K17" <br/><small> Congratulations Our MCA students clinched the overalls at National Level IT Fest - "Enthios 2K17" at Jyothi Nivas College, Bangalore on 21st and 22nd Februray 2017..
+                <a href="events/events.php?mode=openlink&year=2017&month=february&url=enthios_2K17.php"><h4> "Enthios 2K17" <br/><small> Congratulations Our MCA students clinched the overalls at National Level IT Fest - "Enthios 2K17" at Jyothi Nivas College, Bangalore on 21st and 22nd Februray 2017..
                 </small></h4></a>
             </div>
         </li>
@@ -25,7 +25,7 @@
             <img src="../../../../highlights/2017/ieee.jpg" class="img-responsive"/>
             <span class="date">16 Jan 2017</span>
             <div class="quote">
-                <a href="../../../../events/events.php?mode=openlink&year=2017&month=january&url=ieee_workshop.php"><h4>IEEE One Day Workshop  <br/><small>The objective of this session was to be familiar with digital convergence for smart life. The sessions focused on topics relevant to both computer science and life science applications..
+                <a href="events/events.php?mode=openlink&year=2017&month=january&url=ieee_workshop.php"><h4>IEEE One Day Workshop  <br/><small>The objective of this session was to be familiar with digital convergence for smart life. The sessions focused on topics relevant to both computer science and life science applications..
                 </small></h4></a>
             </div>
         </li>
@@ -35,7 +35,7 @@
             <img src="../../../../highlights/2017/32_vichaarmantahn.jpg" class="img-responsive"/>
             <span class="date">21 Jan 2017</span>
             <div class="quote">
-                <a href="../../../../events/events.php?mode=openlink&year=2017&month=january&url=32edition_vichaarmanthan.php"><h4> 32<sup>nd</sup> edition of<br> Vichaarmanthan<br/><small>The event was titled on the theme 'Fundamental Duties'. The Honorable Justice emphasized the importance of the duties and responsibilities associated with being a citizen of India. ..
+                <a href="events/events.php?mode=openlink&year=2017&month=january&url=32edition_vichaarmanthan.php"><h4> 32<sup>nd</sup> edition of<br> Vichaarmanthan<br/><small>The event was titled on the theme 'Fundamental Duties'. The Honorable Justice emphasized the importance of the duties and responsibilities associated with being a citizen of India. ..
                 </small></h4></a>
             </div>
         </li>
@@ -45,7 +45,7 @@
             <img src="../../../../highlights/2017/research_collqium.jpg" class="img-responsive"/>
             <span class="date">26 Nov 2016</span>
             <div class="quote">
-                <a href="../../../../events/events.php?mode=openlink&year=2016&month=november&url=research_colloquium.php"><h4>Research Colloquium Writing of<br> Research Proposal and<br> Funding Agencies<br/><small>Research colloquium is a programme organized by the department of Computer which is conducted once in every semester since 2011..
+                <a href="events/events.php?mode=openlink&year=2016&month=november&url=research_colloquium.php"><h4>Research Colloquium Writing of<br> Research Proposal and<br> Funding Agencies<br/><small>Research colloquium is a programme organized by the department of Computer which is conducted once in every semester since 2011..
                 </small></h4></a>
             </div>
         </li>
@@ -55,7 +55,7 @@
             <img src="../../../../highlights/2017/workshop_r.jpg" class="img-responsive"/>
             <span class="date">26-27 Aug 2016</span>
             <div class="quote">
-                <a href="../../../../events/events.php?mode=openlink&year=2016&month=august&url=data_analysis_using_r.php"><h4>National Level Workshop on Data Analysis Using R  <br/><small>The Department of Computer Science (PG) conducted a Two day National Level workshop on "Data Analysis Using R" on 26th and 27th August 2016..
+                <a href="events/events.php?mode=openlink&year=2016&month=august&url=data_analysis_using_r.php"><h4>National Level Workshop on Data Analysis Using R  <br/><small>The Department of Computer Science (PG) conducted a Two day National Level workshop on "Data Analysis Using R" on 26th and 27th August 2016..
                 </small></h4></a>
             </div>
         </li>
