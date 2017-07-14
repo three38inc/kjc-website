42ec217d4e89b23f128f3aa28329e184131e7b68
PraveenKumarkjc <pravisweets@gmail.com>
2017-07-14 16:08:34 +0530

    changes

--- home/php/upcoming-events.php
+++ home/php/upcoming-events.php
@@ -629,7 +629,7 @@
                         </table>
                     </li>
                     
-                    <li class="news-item">
+                   <!-- <li class="news-item">
                         <table cellpadding="4">
                             <tr>
                                 <td style="width: 60px; text-align: center; ">
@@ -648,7 +648,7 @@
 								</td>
 							</tr>
                         </table>
-                    </li>
+                    </li>-->
 					
 					<li class="news-item">
                         <table cellpadding="4">
