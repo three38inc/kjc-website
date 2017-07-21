8133515850d544df56dd70d67ef48ada3c23a231
Kurian George Cheripurathu <akmkurian@gmail.com>
2017-07-21 15:37:51 +0530

    ICONS- DENARY

--- academics/College-Arts-Science-Commerce/php/deanary.php
+++ academics/College-Arts-Science-Commerce/php/deanary.php
@@ -3,11 +3,12 @@
     <span style="color: #fff;">&nbsp;Kristu Jayanti <strong> College of  Arts, Science And Commerce</strong></span>
     </h4></center>
 <div class="row">
-
     <div class="col-lg-4 deanary" id="falSci">
-        <div class="iconDiv">
-            <i class="fa fa-folder-open-o" aria-hidden="true"></i>
-<!--            <img src="images/science.png" class="img-responsive" width="20"/>-->
+        <div class="circle">
+            <div class="halfCir"></div>
+            <div class="innerCircle">
+                <img src="images/science.png" class="circleImg" />
+            </div>
         </div>
         <h3>Faculty of Science</h3>
         <p>Faculty of Science offers MCA, BCA, BSc CSME, BSc CSMS, BSc Biotechnology, Microbiology and MSc Biotechnology, Microbiology  ..</p>
@@ -15,8 +16,11 @@
     </div>
 
     <div class="col-lg-4 deanary" id="falCom">
-        <div class="iconDiv">
-            <i class="fa fa-folder-open-o" aria-hidden="true"></i>
+        <div class="circle">
+            <div class="halfCir"></div>
+            <div class="innerCircle">
+                <img src="images/commerce-management.png" class="circleImg" />
+            </div>
         </div>
         <h3>Faculty Of Commerce &amp; <br>  Management  </h3>
         <p>Faculty of Commerce &amp; Management offers M.Com, B.Com, B.Com Professional, BBA, B.Com Tourism ..</p>
@@ -24,8 +28,11 @@
     </div>
 
     <div class="col-lg-4 deanary" id="falHum">
-        <div class="iconDiv">
-            <i class="fa fa-folder-open-o" aria-hidden="true"></i>
+        <div class="circle">
+            <div class="halfCir"></div>
+            <div class="innerCircle">
+                <img src="images/humanities.png" class="circleImg" />
+            </div>
         </div>
         <h3>Faculty Of Humanities</h3>
         <p>The Faculty of Humanities offers Social Work, Psychology, Mass Communication, Literature and Languages, History, Culture and Political Science..</p>
