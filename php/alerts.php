5bf21f7b68f5a55089cad25761dee7cf228ed304
PraveenKumarkjc <pravisweets@gmail.com>
2017-07-24 17:43:53 +0530

    changes

--- php/alerts.php
+++ php/alerts.php
@@ -1,5 +1,5 @@
 <div class="newsAlert">
-	<marquee onmouseover="stop();"  onmouseout="start();"> UG Revaluation Results published online on 22-7-17. Students can view the same through the ERP solution (TCS ion). |   Inauguration of Academic Year - commencement of I sem Law programmes on 31<sup>st</sup>, July 2017 </marquee>
+	<marquee onmouseover="stop();"  onmouseout="start();"> II sem BA / B.SC / B.COM / BBA / BCA Supplementry results are announced. Students can check results on ERP Solutions. | UG Revaluation Results published online on 22-7-17. Students can view the same through the ERP solution (TCS ion). |   Inauguration of Academic Year - commencement of I sem Law programmes on 31<sup>st</sup>, July 2017 </marquee>
     <div class='hide-btn'>
         <i class="fa fa-angle-double-left"></i>
     </div>
