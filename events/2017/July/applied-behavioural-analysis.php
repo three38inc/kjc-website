19e24eeaa2d65ede46194472a49ec1dd3ee3f392
PraveenKumarkjc <pravisweets@gmail.com>
2017-07-28 10:46:19 +0530

    updates

---
+++ events/2017/July/applied-behavioural-analysis.php
@@ -0,0 +1,55 @@
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 event-tab">
+	<div class="panel panel-primary">
+		<div class="panel-heading">
+ Workshop on ‘Writing for Newspapers - Professional Perspectives'
+
+            <b class="panel-title pull-right">July 29<sup>th</sup>, Aug 5, 19, 26 2017</b>
+            <div style="clear:both"></div>
+        </div>
+		<div class="panel-body">
+		
+
+            <p align=justify><b>Guest Lecture Series on Applied Behavioural Analysis</b><br>
+Resource Person – Ms. Kinnari Bhatt, Director, DAKSH ABA & Autism Services<br>
+Participants – Final year M.Sc. Students <br>
+                Venue – Room No: 512</p>
+            <p><b>Date and time -  </b></p>
+                <table border="1" cellspacing="0" cellpadding="0" class="table table-bordered table-hover">
+  <tr>
+    <td width="67"><p align="center">Sl. No</p></td>
+    <td width="126"><p align="center">Dates</p></td>
+    <td width="186"><p align="center">Timings</p></td>
+  </tr>
+  <tr>
+    <td width="67"><p align="center">1.</p></td>
+    <td width="126"><p align="center">July 29th</p></td>
+    <td width="186"><p align="center">9 AM – 10.50 AM</p></td>
+  </tr>
+  <tr>
+    <td width="67"><p align="center">2.</p></td>
+    <td width="126"><p align="center">August 5th</p></td>
+    <td width="186"><p align="center">9 AM – 12.50 PM</p></td>
+  </tr>
+  <tr>
+    <td width="67"><p align="center">3.</p></td>
+    <td width="126"><p align="center">August 19th</p></td>
+    <td width="186"><p align="center">9 AM – 12.50 PM</p></td>
+  </tr>
+  <tr>
+    <td width="67"><p align="center">4.</p></td>
+    <td width="126"><p align="center">August 26th</p></td>
+    <td width="186"><p align="center">9 AM – 12.50 PM</p></td>
+  </tr>
+</table>
+
+
+			<!--<img src="images/kristu-jayanti-college-pradeep.jpg">-->
+
+			
+			<!--<center><a href="https://www.facebook.com/pg/KristuJayantiCollege/photos/?tab=album&album_id=1390413994370802" target="_blank" class="btn btn-primary btn-sm">PHOTO GALLERY</a></center>-->
+			
+			
+			
+		</div>
+	</div>
+</div>
--- home/php/upcoming-events.php
+++ home/php/upcoming-events.php
@@ -917,6 +917,27 @@
                         <table cellpadding="4">
                             <tr>
                                 <td style="width: 60px; text-align: center; ">
+                                <p style="font-size: 1.2em;font-weight: bold;">JUL-AUG</p>
+                                <p style="font-size: 1.7em;color: #2980b9;margin-top: -20px;padding: 7px 0px;">29..</p>
+                                <p style="font-size: 1em;color: #2980b9;margin-top: -20px;padding: 0;">2017</p></td>
+								<td>
+								<p style="margin-top: 2px; margin-bottom: 0px;">
+								Department of Psychology Proprium Series Guest Lecture Series on Applied Behavioural Analysis
+
+								</p>
+								<a href="../events/events.php?mode=openlink&year=2017&month=july&url=applied-behavioural-analysis.php" class="read-more">
+                                    Read More 
+                                    <i class='fa fa-angle-double-right'></i>
+								</a>
+								</td>
+							</tr>
+                        </table>
+                    </li>
+                    
+                    <li class="news-item">
+                        <table cellpadding="4">
+                            <tr>
+                                <td style="width: 60px; text-align: center; ">
                                 <p style="font-size: 2em;font-weight: bold;">JUL</p>
                                 <p style="font-size: 2em;color: #2980b9;margin-top: -20px;padding: 0;">31</p>
                                 <p style="font-size: 1em;color: #2980b9;margin-top: -20px;padding: 0;">2017</p></td>
